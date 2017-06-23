063f6ede9eddd7fa3f30e4af8fe1b2e2e242ed6b
windancer79 <8199705@qq.com>
2017-06-23 10:35:00 +0800

    增加提示框

---
+++ js/msgbox.js
@@ -0,0 +1,25 @@
+$.msgbox = function(s,callback){
+     new Noty({
+                        text: s,
+                        type: 'success',
+                        layout:'topCenter',
+                        theme :'relax',
+                        timeout: 1000,
+                        callbacks: {
+                            afterClose: callback
+                        }
+                    }).show();
+}
+
+$.alert = function(s,callback){
+     new Noty({
+                        text: s,
+                        type: 'alert',
+                        layout:'topCenter',
+                        theme :'relax',
+                        timeout: 1000,
+                        callbacks: {
+                            afterClose: callback
+                        }
+                    }).show();
+}
