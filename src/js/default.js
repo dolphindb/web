2be5882fcfc0e388a0c37e9f3900abf1ecf00349
林亮 <8199705@qq.com>
2018-03-22 14:30:51 +0800

    update tips

--- src/js/default.js
+++ src/js/default.js
@@ -501,7 +501,7 @@ function LoadTable(nodeList) {
             }, {
                 text: 'LastMsgLatency',
                 key: 'lastMsgLatency',
-                remind: 'the last latency of streaming message',
+                remind: 'the latency of last received streaming message',
                 sorting: '',
                 width: 90,
                 template: function(lastMsgLatency, rowObject) {
@@ -511,7 +511,7 @@ function LoadTable(nodeList) {
             {
                 text: 'CumMsgLatency',
                 key: 'cumMsgLatency',
-                remind: 'the cum latency of streaming message',
+                remind: 'the weighed average latency of all recieved message since the subscription',
                 sorting: '',
                 width: 90,
                 template: function(cumMsgLatency, rowObject) {
