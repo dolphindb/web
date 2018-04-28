f1b6955a94ad4353fc2cdb54571112e9bae4c35d
liang.lin <8199705@qq.com>
2018-04-28 11:07:38 +0800

    add lastMinuteReadVolume

--- src/js/default.js
+++ src/js/default.js
@@ -435,7 +435,16 @@ function LoadTable(nodeList) {
                 template: function(diskWirtePerMinute, rowObject) {
                     return fmoney((diskWirtePerMinute / (1024 * 1024)), 1) + " MB";
                 }
-            }, {
+            },  {
+                text: 'LastMinuteReadVolume',
+                key: 'lastMinuteReadVolume',
+                remind: 'the size of disk reading last minute',
+                sorting: '',
+                width: 90,
+                template: function(lastMinuteReadVolume, rowObject) {
+                    return fmoney((lastMinuteReadVolume / (1024 * 1024)), 1) + " MB";
+                }
+            },  {
                 text: 'Workers',
                 key: 'workerNum',
                 remind: 'number of job workers',
