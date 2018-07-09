60d915aa0ab1bb3dbc3472318ec3f97419b93856
liang.lin <8199705@qq.com>
2018-07-09 13:11:46 +0800

    bug fix:  sessionid changed call agentApi
    call webapi without sessionId update

--- src/js/CallWebApi.js
+++ src/js/CallWebApi.js
@@ -8,7 +8,7 @@ function CallWebApi(apiurl, paramstr, sucfunc, errfunc, customOption) {
     if (typeof CurrentSessionID === 'undefined') {
         var CurrentSessionID = 0;
     }
-    if (localStorage.getItem(session_storage_id) == null||localStorage.getItem(session_storage_id)=="") {
+    if (localStorage.getItem(session_storage_id) == null || localStorage.getItem(session_storage_id) == "") {
         paramstr['sessionID'] = CurrentSessionID;
         localStorage.setItem(session_storage_id, CurrentSessionID)
     } else {
@@ -17,29 +17,29 @@ function CallWebApi(apiurl, paramstr, sucfunc, errfunc, customOption) {
     }
     var d = JSON.stringify(paramstr);
 
-    var slash = apiurl.charAt(apiurl.length -1);
+    var slash = apiurl.charAt(apiurl.length - 1);
 
-    var sessId = CurrentSessionID==0?"":CurrentSessionID;
-    if(slash=="/"){
+    var sessId = CurrentSessionID == 0 ? "" : CurrentSessionID;
+    if (slash == "/") {
         apiurl = apiurl + sessId;
-    }else{
+    } else {
         apiurl = apiurl + "/" + sessId;
     }
-    
+
     var option = {
         url: apiurl,
         async: true,
         data: d,
         type: "POST",
         dataType: "json",
-        success: function(data, status, xhr) {
+        success: function (data, status, xhr) {
 
             CurrentSessionID = data["sessionID"];
             localStorage.setItem(session_storage_id, CurrentSessionID);
 
             if (sucfunc) sucfunc(data);
         },
-        error: function(XMLHttpRequest, textStatus, errorThrown) {
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
             if (errfunc)
                 errfunc(errorThrown);
             else
@@ -59,7 +59,7 @@ function CallWebApiSync(apiurl, paramstr) {
     if (typeof CurrentSessionID === 'undefined') {
         var CurrentSessionID = 0;
     }
-    if (localStorage.getItem(session_storage_id) == null||localStorage.getItem(session_storage_id)=="") {
+    if (localStorage.getItem(session_storage_id) == null || localStorage.getItem(session_storage_id) == "") {
         paramstr['sessionID'] = CurrentSessionID;
         localStorage.setItem(session_storage_id, CurrentSessionID)
     } else {
@@ -69,12 +69,12 @@ function CallWebApiSync(apiurl, paramstr) {
     paramstr['sessionID'] = CurrentSessionID;
     var d = JSON.stringify(paramstr);
 
-    var slash = apiurl.charAt(apiurl.length -1);
+    var slash = apiurl.charAt(apiurl.length - 1);
 
-    var sessId = CurrentSessionID==0?"":CurrentSessionID;
-    if(slash=="/"){
+    var sessId = CurrentSessionID == 0 ? "" : CurrentSessionID;
+    if (slash == "/") {
         apiurl = apiurl + sessId;
-    }else{
+    } else {
         apiurl = apiurl + "/" + sessId;
     }
     var re = $.ajax({
@@ -88,5 +88,77 @@ function CallWebApiSync(apiurl, paramstr) {
     var reobj = JSON.parse(re.responseText);
     CurrentSessionID = reobj["sessionID"];
     localStorage.setItem(session_storage_id, CurrentSessionID);
+    return reobj
+};
+
+function CallWebApiWithoutSession(apiurl, paramstr, sucfunc, errfunc, customOption) {
+
+    if (typeof elem === 'undefined')
+        elem = $('#execute-spin');
+    CurrentSessionID = paramstr['sessionID'];
+    if (typeof CurrentSessionID === 'undefined') {
+        var CurrentSessionID = 0;
+    }
+    var d = JSON.stringify(paramstr);
+
+    var slash = apiurl.charAt(apiurl.length - 1);
+
+    var sessId = CurrentSessionID == 0 ? "" : CurrentSessionID;
+    if (slash == "/") {
+        apiurl = apiurl + sessId;
+    } else {
+        apiurl = apiurl + "/" + sessId;
+    }
+
+    var option = {
+        url: apiurl,
+        async: true,
+        data: d,
+        type: "POST",
+        dataType: "json",
+        success: function (data, status, xhr) {
+            if (sucfunc) sucfunc(data);
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            if (errfunc)
+                errfunc(errorThrown);
+            else
+                throw errorThrown;
+        }
+    }
+
+    if (customOption)
+        $.extend(option, customOption);
+
+    $.ajax(option);
+};
+
+function CallWebApiSyncWithoutSession(apiurl, paramstr) {
+    if (typeof elem === 'undefined')
+        elem = $('#execute-spin');
+
+    CurrentSessionID = paramstr['sessionID'];
+    if (typeof CurrentSessionID === 'undefined') {
+        var CurrentSessionID = 0;
+    }
+    var d = JSON.stringify(paramstr);
+
+    var slash = apiurl.charAt(apiurl.length - 1);
+
+    var sessId = CurrentSessionID == 0 ? "" : CurrentSessionID;
+    if (slash == "/") {
+        apiurl = apiurl + sessId;
+    } else {
+        apiurl = apiurl + "/" + sessId;
+    }
+    var re = $.ajax({
+        url: apiurl,
+        async: false,
+        data: d,
+        timeout: 30000,
+        type: "POST",
+        dataType: "json"
+    });
+    var reobj = JSON.parse(re.responseText);
     return reobj
 };
