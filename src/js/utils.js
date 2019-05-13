1c62b59da3a6016cd66912765c3b429f49260d77
L.Lin <8199705@qq.com>
2019-05-13 12:12:28 +0800

    fix decode bug

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,7 +1,7 @@
 $.getUrlParam = function(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
     var r = window.location.search.substr(1).match(reg);
-    if (r !== null) return unescape(r[2]);
+    if (r !== null) return decodeURI(r[2]);
     return null;
 }
 
