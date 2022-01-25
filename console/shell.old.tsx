affe2c7617a25ce9afc55fc66e963a3b48f9b07e
Hongfei Shen <hongfei.shen@dolphindb.com>
2022-01-25 14:17:26 +0800

    optimize statistics;

--- console/shell.old.tsx
+++ console/shell.old.tsx
@@ -19,14 +19,14 @@ export function ShellOld () {
                 color: 'rgb(106, 90, 205)'
             }}
         >
-            <Descriptions.Item label={t('连接')}>{node.connectionNum} | {node.maxConnections}</Descriptions.Item>
             <Descriptions.Item label={t('内存使用量')}>{Number(node.memoryUsed).to_fsize_str()}</Descriptions.Item>
             <Descriptions.Item label={t('CPU 使用量')}>{node.cpuUsage.toFixed(1)}%</Descriptions.Item>
+            <Descriptions.Item label={t('当前连接 | 最大连接')}>{node.connectionNum} | {node.maxConnections}</Descriptions.Item>
             <Descriptions.Item label={t('硬盘读取 | 硬盘写入')}>{node.diskReadRate} | {node.diskWriteRate}</Descriptions.Item>
             <Descriptions.Item label={t('网络接收 | 网络发送')}>{node.networkRecvRate} | {node.networkSendRate}</Descriptions.Item>
             <Descriptions.Item label={t('排队作业 | 运行作业')}>{node.queuedJobs} | {node.runningJobs}</Descriptions.Item>
             <Descriptions.Item label={t('排队任务 | 运行任务')}>{node.queuedTasks} | {node.runningTasks}</Descriptions.Item>
-            <Descriptions.Item label='Worker'>{node.workerNum}</Descriptions.Item>
+            <Descriptions.Item label='Workers'>{node.workerNum}</Descriptions.Item>
         </Descriptions> }
         
         <iframe src='./nodedetail.html' width='100%' />
