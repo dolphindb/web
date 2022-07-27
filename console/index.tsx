5c79f20b567d1f4f1289212aab216bb7c50162e7
huaiyu <huilin.gong@dolphindb.com>
2022-07-27 12:01:41 +0800

    feat: add more status info

--- console/index.tsx
+++ console/index.tsx
@@ -10,29 +10,30 @@ import { default as React, useEffect } from 'react'
 import { createRoot as create_root } from 'react-dom/client'
 
 import {
-    Layout, 
-    Menu,
-    ConfigProvider,
-    Dropdown,
-    Avatar,
-    Typography,
-    Tag,
-    Popover,
-    Descriptions,
-    Badge,
-    Card
-} from 'antd'
+  Layout,
+  Menu,
+  ConfigProvider,
+  Dropdown,
+  Avatar,
+  Typography,
+  Tag,
+  Popover,
+  Descriptions,
+  Badge,
+  Card,
+  Tooltip,
+} from 'antd';
 import {
-    default as Icon,
-    AppstoreOutlined, 
-    DatabaseOutlined, 
-    DownOutlined, 
-    LogoutOutlined, 
-    TableOutlined,
-    UserOutlined,
-    DoubleLeftOutlined,
-    DoubleRightOutlined,
-    SyncOutlined,
+  default as Icon,
+  AppstoreOutlined,
+  DatabaseOutlined,
+  DownOutlined,
+  LogoutOutlined,
+  TableOutlined,
+  UserOutlined,
+  DoubleLeftOutlined,
+  DoubleRightOutlined,
+  SyncOutlined,
 } from '@ant-design/icons'
 import zh from 'antd/lib/locale/zh_CN.js'
 import en from 'antd/lib/locale/en_US.js'
@@ -66,64 +67,87 @@ const locales = { zh, en, ja, ko }
 
 function DolphinDB () {
     const { inited } = model.use(['inited'])
-    
-    useEffect(() => {
+
+  useEffect(() => {
         model.init()
     }, [ ])
-    
+
     if (!inited)
         return null
-    
+
     return <ConfigProvider locale={locales[language] as any} autoInsertSpaceInButton={false}>
         <Layout className='root-layout'>
             <Layout.Header className='header'>
-                <DdbHeader />
-            </Layout.Header>
+          <DdbHeader />
+        </Layout.Header>
             <Layout className='body'>
-                <DdbSider />
+          <DdbSider />
                 <Layout.Content className='view'>
-                    <DdbContent />
-                </Layout.Content>
-            </Layout>
+            <DdbContent />
+          </Layout.Content>
         </Layout>
+      </Layout>
     </ConfigProvider>
 }
 
 
 function DdbHeader () {
     const { logined, username, node_alias, version, license } = model.use(['logined', 'username', 'node_alias', 'version', 'license'])
-    
-    const authorizations = {
+
+  const authorizations = {
         trial: t('试用版'),
         community: t('社区版'),
         commercial: t('商业版')
     }
-    
-    useEffect(() => {
+
+  useEffect(() => {
         if (!node_alias)
             return
         document.title = `DolphinDB - ${node_alias}`
     }, [node_alias])
-    
+
     return <>
         <img className='logo' src='./ddb.svg' />
-        
+
         <div className='padding' />
-        
-        <div>
-            <Popover
-                placement='bottomLeft'
-                content={<Perf />}
+
+      <div>
+        <Popover
+          placement="bottomLeft"
+          content={
+            <Card
+              size="small"
+              title={t("状态")}
+              extra={
+                <div
+                  className="refresh"
+                  onClick={() => {
+                    model.get_cluster_perf();
+                  }}
+                >
+                  <Tooltip title={t("刷新")} color={"grey"}>
+                    <SyncOutlined className="icon" />
+                  </Tooltip>
+                </div>
+              }
             >
-                <Tag className='node-info' color='#f2f2f2'>{t('状态')}</Tag>
-            </Popover>
-        </div>
-        
+              <div className="status-description">
+                <Perf />
+              </div>
+            </Card>
+          }
+        >
+          <Tag className="node-info" color="#f2f2f2">
+            {t("状态")}
+          </Tag>
+        </Popover>
+      </div>
+
         {
             license && <div>
-                <Popover
+          <Popover
                     placement='bottomLeft'
-                    content={
+            content={
                         license ? <div>
                             <Badge.Ribbon text={`V${version}`} color='#b5b5b5'>
                                 <Card size='small' title={ authorizations[license.authorization] || license.authorization }>
@@ -138,190 +162,260 @@ function DdbHeader () {
                                     <Descriptions.Item label={t('每节点最大可用内存')}>{license.maxMemoryPerNode}</Descriptions.Item>
                                     <Descriptions.Item label={t('每节点最大可用核数')}>{license.maxCoresPerNode}</Descriptions.Item>
                                     <Descriptions.Item label={t('最大节点数')}>{license.maxNodes}</Descriptions.Item>
-                                </Descriptions>
-                                </Card>
-                            </Badge.Ribbon>
+                      </Descriptions>
+                    </Card>
+                  </Badge.Ribbon>
                         </div> : null
-                    }
-                >
+            }
+          >
                     <Tag className='license' color='#f2f2f2'>{authorizations[license.authorization] || license.authorization}</Tag>
-                </Popover>
-            </div>
+          </Popover>
+        </div>
         }
 
         <div className='user'>
-            <Dropdown
-                overlay={
-                    <Menu
+        <Dropdown
+          overlay={
+            <Menu
                         className='menu'
-                        items={[
+              items={[
                             logined ?
                                 {
                                     label: <a
                                             className='logout'
-                                            onClick={() => {
+                        onClick={() => {
                                                 model.logout()
-                                            }}
+                        }}
                                         >{t('注销')}</a>,
                                     key: 'logout',
                                     icon: <LogoutOutlined />
-                                }
+                  }
                             :
                                 {
                                     label: <a
                                             className='login'
-                                            onClick={() => {
+                        onClick={() => {
                                                 model.set({ view: 'login' })
-                                            }}
+                        }}
                                         >{t('登录')}</a>,
                                     key: 'login',
                                     icon: <LogoutOutlined />
                                 }
                             ]
                         } 
-                    />
-                }
-            >
+            />
+          }
+        >
                 <a className='username'>
                     <Avatar className='avatar' icon={<UserOutlined />} size='small' /> {username} <DownOutlined />
-                </a>
-            </Dropdown>
-        </div>
+          </a>
+        </Dropdown>
+      </div>
     </>
 }
 
 function Perf() {
-    const { node } = model.use(['node'])
+  const { node } = model.use(["node"]);
 
-    if (!node)
-        return null
+  if (!node) return null;
 
-    return <div className='perf'>
-        <Descriptions
-            className='table'
-            column={9}
-            bordered
-            size='small'
-            layout='vertical'
-        >
-            <Descriptions.Item label={t('内存已用 (已分配) / 最大可用')}>{Number(node.memoryUsed).to_fsize_str()} ({Number(node.memoryAlloc).to_fsize_str()}) / {node.maxMemSize} GB</Descriptions.Item>
-            <Descriptions.Item label={t('CPU 用量 (平均负载)')}>{node.cpuUsage.toFixed(1)}% ({node.avgLoad.toFixed(1)})</Descriptions.Item>
-            <Descriptions.Item label={t('当前连接 | 最大连接')}>{node.connectionNum} | {node.maxConnections}</Descriptions.Item>
-            <Descriptions.Item label={t('硬盘读取 | 硬盘写入')}>{Number(node.diskReadRate).to_fsize_str()}/s | {Number(node.diskWriteRate).to_fsize_str()}/s</Descriptions.Item>
-            <Descriptions.Item label={t('网络接收 | 网络发送')}>{Number(node.networkRecvRate).to_fsize_str()}/s | {Number(node.networkSendRate).to_fsize_str()}/s</Descriptions.Item>
-            <Descriptions.Item label={t('排队作业 | 运行作业')}>{node.queuedJobs} | {node.runningJobs}</Descriptions.Item>
-            <Descriptions.Item label={t('排队任务 | 运行任务')}>{node.queuedTasks} | {node.runningTasks}</Descriptions.Item>
-            <Descriptions.Item label='Workers'>{node.workerNum} | {node.executorNum} | {node.jobLoad}</Descriptions.Item>
-        </Descriptions>
-
-        <div
-            className='refresh'
-            onClick={() => {
-                model.get_cluster_perf()
-            }}
-        >
-            <SyncOutlined className='icon' />
-            <div className='text'>{t('刷新')}</div>
-        </div>
+  return (
+    <div className="perf">
+      <Descriptions className="table" column={1} bordered size="small">
+        <Descriptions.Item label={t("内存已用")}>
+          {Number(node.memoryUsed).to_fsize_str()}
+        </Descriptions.Item>
+        <Descriptions.Item label={t("内存已分配")}>
+          {Number(node.memoryAlloc).to_fsize_str()}
+        </Descriptions.Item>
+        <Descriptions.Item label={t("内存最大可用")}>
+          {node.maxMemSize} GB
+        </Descriptions.Item>
+        <Descriptions.Item label={t("CPU用量")}>
+          {node.cpuUsage.toFixed(1)}%
+        </Descriptions.Item>
+        <Descriptions.Item label={t("CPU平均负载")}>
+          ({node.avgLoad.toFixed(1)})
+        </Descriptions.Item>
+        <Descriptions.Item label={t("当前连接")}>
+          {node.connectionNum}
+        </Descriptions.Item>
+        <Descriptions.Item label={t("最大连接")}>
+          {node.maxConnections}
+        </Descriptions.Item>
+        <Descriptions.Item label={t("硬盘读取")}>
+          {Number(node.diskReadRate).to_fsize_str()}/s
+        </Descriptions.Item>
+        <Descriptions.Item label={t("硬盘写入")}>
+          {Number(node.diskWriteRate).to_fsize_str()}/s
+        </Descriptions.Item>
+        <Descriptions.Item label={t("磁盘剩余容量")}>
+          {Number(node.diskFreeSpace).to_fsize_str()}
+        </Descriptions.Item>
+        <Descriptions.Item label={t("磁盘总容量")}>
+          {Number(node.diskCapacity).to_fsize_str()}
+        </Descriptions.Item>
+        <Descriptions.Item label={t("磁盘可用空间占比")}>
+          {node.diskFreeSpaceRatio.toFixed(2)}%
+        </Descriptions.Item>
+        <Descriptions.Item label={t("网络接收速率")}>
+          {Number(node.networkRecvRate).to_fsize_str()}/s
+        </Descriptions.Item>
+        <Descriptions.Item label={t("网络发送速率")}>
+          {Number(node.networkSendRate).to_fsize_str()}/s
+        </Descriptions.Item>
+        <Descriptions.Item label={t("排队作业")}>
+          {node.queuedJobs}{" "}
+        </Descriptions.Item>
+        <Descriptions.Item label={t("运行作业")}>
+          {node.runningJobs}
+        </Descriptions.Item>
+        <Descriptions.Item label={t("作业负载")}>
+          {node.jobLoad}
+        </Descriptions.Item>
+        <Descriptions.Item label={t("排队任务")}>
+          {node.queuedTasks}
+        </Descriptions.Item>
+        <Descriptions.Item label={t("运行任务")}>
+          {node.runningTasks}
+        </Descriptions.Item>
+        <Descriptions.Item label={t("worker数")}>
+          {node.workerNum}
+        </Descriptions.Item>
+        <Descriptions.Item label={t("excutor数")}>
+          {node.executorNum}
+        </Descriptions.Item>
+        <Descriptions.Item label={t("前10个查询耗费中间值")}>
+          {Number(node.medLast10QueryTime).to_fsize_str()} s
+        </Descriptions.Item>
+        <Descriptions.Item label={t("前10个查询耗费最大值")}>
+          {Number(node.maxLast10QueryTime).to_fsize_str()} s
+        </Descriptions.Item>
+        <Descriptions.Item label={t("前100个查询耗费中间值")}>
+          {Number(node.medLast100QueryTime).to_fsize_str()} s
+        </Descriptions.Item>
+        <Descriptions.Item label={t("前100个查询耗费最大值")}>
+          {Number(node.maxLast100QueryTime).to_fsize_str()} s
+        </Descriptions.Item>
+        <Descriptions.Item label={t("前一分钟读磁盘容量")}>
+          {Number(node.lastMinuteWriteVolume).to_fsize_str()}
+        </Descriptions.Item>
+        <Descriptions.Item label={t("前一分钟写磁盘容量")}>
+          {Number(node.lastMinuteWriteVolume).to_fsize_str()}
+        </Descriptions.Item>
+        <Descriptions.Item label={t("前一分钟接收字节数")}>
+          {Number(node.lastMinuteNetworkRecv).to_fsize_str()}
+        </Descriptions.Item>
+        <Descriptions.Item label={t("前一分钟发送字节数")}>
+          {Number(node.lastMinuteNetworkSend).to_fsize_str()}
+        </Descriptions.Item>
+        {Number(node.lastMsgLatency)>=0?<Descriptions.Item label={t("前一批消息延时")}>
+          {Number(node.lastMsgLatency).to_fsize_str()} s
+        </Descriptions.Item>:null}
+        {Number(node.cumMsgLatency)>=0?<Descriptions.Item label={t("所有消息平均延时")}>
+          {Number(node.cumMsgLatency).to_fsize_str()} s
+        </Descriptions.Item>:null}
+      </Descriptions>
     </div>
+  );
 }
 
 function DdbSider () {
     const { view, node_type, collapsed } = model.use(['view', 'node_type', 'collapsed'])
-    
+
     return <Layout.Sider
-        width={130}
+      width={130}
         className='sider'
         theme='light'
-        collapsible
-        collapsedWidth={50}
-        collapsed={collapsed}
+      collapsible
+      collapsedWidth={50}
+      collapsed={collapsed}
         trigger={<div className={`collapse-trigger ${collapsed ? 'collapsed' : 'expand'}`}>
             {collapsed ? <DoubleRightOutlined className='collapse-icon' /> : <DoubleLeftOutlined className='collapse-icon' />}
             {!collapsed && <Text className='text' ellipsis>{t('收起侧栏')}</Text>}
         </div>}
-        onCollapse={(collapsed, type) => {
+      onCollapse={(collapsed, type) => {
             localStorage.setItem(
                 storage_keys.collapsed,
                 String(collapsed)
             )
             model.set({ collapsed })
-        }}
+      }}
     >
-        <Menu
+      <Menu
             className='menu'
             mode='inline'
             theme='light'
-            selectedKeys={[view]}
-            onSelect={({ key }) => {
+        selectedKeys={[view]}
+        onSelect={({ key }) => {
                 model.set({ view: key as DdbModel['view'] })
-            }}
-            inlineIndent={20}
-            items={[
-                // {
-                //     key: 'overview',
-                //     icon: <AppstoreOutlined />,
-                //     label: t('总览'),
-                // },
+        }}
+        inlineIndent={20}
+        items={[
+          // {
+          //     key: 'overview',
+          //     icon: <AppstoreOutlined />,
+          //     label: t('总览'),
+          // },
                 ... node_type === NodeType.controller ? [{
                     key: 'cluster',
                     icon: <MenuIcon view='cluster' />,
                     label: t('集群总览'),
                 }] : [ ],
-                {
+              {
                     key: 'shell',
                     icon: <MenuIcon view='shell' />,
                     label: t('交互编程'),
-                },
-                // {
-                //     key: 'data',
-                //     label: t('数据'),
-                //     icon: <DatabaseOutlined />,
-                //     children: [
-                //         {
-                //             key: 'table',
-                //             icon: <TableOutlined />,
-                //             label: t('数据表'),
-                //         },
-                //     ]
-                // },
-                {
+          },
+          // {
+          //     key: 'data',
+          //     label: t('数据'),
+          //     icon: <DatabaseOutlined />,
+          //     children: [
+          //         {
+          //             key: 'table',
+          //             icon: <TableOutlined />,
+          //             label: t('数据表'),
+          //         },
+          //     ]
+          // },
+          {
                     key: 'job',
                     icon: <MenuIcon view='job' />,
                     label: t('作业管理'),
-                },
+          },
                 ... (node_type === NodeType.controller || node_type === NodeType.single) ? [{
                     key: 'dfs',
                     icon: <MenuIcon view='dfs' />,
                     label: t('文件系统'),
                 }] : [ ],
-                {
+              {
                     key: 'log',
                     icon: <MenuIcon view='log' />,
                     label: t('日志查看'),
-                },
-            ]}
-        />
+          },
+        ]}
+      />
     </Layout.Sider>
 }
 
 const views = {
-    login: Login,
-    cluster: Cluster,
-    shell: Shell,
-    job: Job,
-    dfs: DFS,
-    log: Log,
+  login: Login,
+  cluster: Cluster,
+  shell: Shell,
+  job: Job,
+  dfs: DFS,
+  log: Log,
 }
 
 function DdbContent () {
     const { view } = model.use(['view'])
-    
+
     const View = views[view]
-    
+
     if (!View)
         return null
-    
+
     return <div className={`view-card ${view}`}>
         <View/>
     </div>
@@ -329,11 +423,11 @@ function DdbContent () {
 
 
 const svgs = {
-    cluster: SvgCluster,
-    job: SvgJob,
-    shell: SvgShell,
-    dfs: SvgDFS,
-    log: SvgLog,
+  cluster: SvgCluster,
+  job: SvgJob,
+  shell: SvgShell,
+  dfs: SvgDFS,
+  log: SvgLog,
 }
 
 function MenuIcon ({ view }: { view: DdbModel['view'] }) {
