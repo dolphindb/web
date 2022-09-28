2f81a6e052264d0b08087920a81f674acb7940e4
huaiyu <huilin.gong@dolphindb.com>
2022-09-28 17:44:54 +0800

    fix: fix dbclick in terminal link

--- console/shell.tsx
+++ console/shell.tsx
@@ -852,7 +852,9 @@ function Term () {
             term.loadAddon(new WebglAddon())
             
             ddb.listeners.push(printer)
-            
+            const xterms = document.getElementsByClassName('xterm')
+            if (xterms.length > 0)
+                xterms[0].dispatchEvent(new Event('mousedown'))
             term.writeln(
                 t('左侧编辑器使用指南:\n') +
                 t('按 Ctrl + E 执行选中代码或光标所在行代码\n') +
