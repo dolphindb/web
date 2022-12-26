d026a515303129c6af668cf7e4fb08846381b6d4
Hongfei Shen <hongfei.shen@dolphindb.com>
2022-12-26 21:58:24 +0800

    log error modal;

--- console/log.tsx
+++ console/log.tsx
@@ -1,7 +1,7 @@
 import './log.sass'
 
 import { default as React, useEffect, useRef, useState } from 'react'
-import { Pagination, Button, message } from 'antd'
+import { Pagination, Button, message, Modal } from 'antd'
 import { ReloadOutlined } from '@ant-design/icons'
 
 import { t } from '../i18n/index.js'
@@ -29,16 +29,19 @@ export function Log () {
     
     async function init () {
         set_index(1)
-        const temp = await get_log_length()
-        const offset = temp > default_length ? temp - default_length : BigInt(0)
-        const length = temp > default_length ? default_length : temp
-        await get_log(length, offset)
+        try {
+            const temp = await get_log_length()
+            const offset = temp > default_length ? temp - default_length : BigInt(0)
+            const length = temp > default_length ? default_length : temp
+            await get_log(length, offset)
+        } catch (error) {
+            Modal.error({ title: error.message })
+            throw error
+        }
     }
     
     async function get_log (length: bigint, offset: bigint) {
-        set_logs(
-            await model.get_server_log(offset, length)
-        )
+        set_logs(await model.get_server_log(offset, length))
     }
     
     async function get_log_length () {
@@ -47,63 +50,55 @@ export function Log () {
         return len
     }
     
-    return <>
-        <div className='list'>
-            <div className='log-title'>
-                <div className='log-name'>
-                    {node_alias} {t('日志')} ({Number(log_length).to_fsize_str()})
-                </div>
-                <div className='space' />
-                <div>
-                    <Button
-                     icon={<ReloadOutlined/>}
-                     onClick={
-                        async () => {
-                            try {
-                                await init()
-                                message.success(t('日志刷新成功'))
-                            } catch {
-                                message.error(t('日志刷新失败'))
-                            }
-                        }
-                    }>{t('刷新')}</Button>
-                </div>
+    return <div className='list'>
+        <div className='log-title'>
+            <div className='log-name'>
+                {node_alias} {t('日志')} ({Number(log_length).to_fsize_str()})
             </div>
-            <div className='log-block' ref={ref}>
-                {logs.map((line, i) => {
-                    let log_type: string
-                    const start = line.indexOf('<')
-                    const end = line.indexOf('>', start)
-                    if (start !== -1 && end !== -1)
-                        log_type = line.substring(start + 1, end)
-                    return <div 
-                        className='log-line' 
-                        style={ colors[log_type] ? { color: colors[log_type] } : null } 
-                        key={`${index}.${i}`}
-                    >
-                        {line}
-                    </div>
-                })}
+            <div className='space' />
+            <div>
+                <Button
+                    icon={<ReloadOutlined/>}
+                    onClick={async () => {
+                        await init()
+                        message.success(t('日志刷新成功'))
+                }}>{t('刷新')}</Button>
             </div>
-            <Pagination
-                className='log-pagination'
-                current={index}
-                showQuickJumper
-                showSizeChanger={false}
-                onChange={(page, page_size) => {
-                    // 重新设置当前显示的 log
-                    const t = log_length - BigInt(page) * default_length
-                    const offset = t > 0 ? t : 0n
-                    const length = t > 0 ? default_length : default_length + t
-                    get_log(length, offset)
-                    set_index(page)
-                    
-                    // log 滑动条滑动到顶端
-                    ref.current?.scrollTo(0, 0)
-                }}
-                pageSize={1}
-                total={Math.ceil(Number(log_length) / Number(default_length))}
-            />
         </div>
-    </>
+        <div className='log-block' ref={ref}>
+            {logs.map((line, i) => {
+                let log_type: string
+                const start = line.indexOf('<')
+                const end = line.indexOf('>', start)
+                if (start !== -1 && end !== -1)
+                    log_type = line.substring(start + 1, end)
+                return <div
+                    className='log-line' 
+                    style={ colors[log_type] ? { color: colors[log_type] } : null } 
+                    key={`${index}.${i}`}
+                >
+                    {line}
+                </div>
+            })}
+        </div>
+        <Pagination
+            className='log-pagination'
+            current={index}
+            showQuickJumper
+            showSizeChanger={false}
+            onChange={(page, page_size) => {
+                // 重新设置当前显示的 log
+                const t = log_length - BigInt(page) * default_length
+                const offset = t > 0 ? t : 0n
+                const length = t > 0 ? default_length : default_length + t
+                get_log(length, offset)
+                set_index(page)
+                
+                // log 滑动条滑动到顶端
+                ref.current?.scrollTo(0, 0)
+            }}
+            pageSize={1}
+            total={Math.ceil(Number(log_length) / Number(default_length))}
+        />
+    </div>
 }
