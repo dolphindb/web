f21194df736e03f7cb6c407f6caa4ad6ffac1f34
Hongfei Shen <hongfei.shen@dolphindb.com>
2023-02-08 17:04:56 +0800

    .

--- console/shell.tsx
+++ console/shell.tsx
@@ -894,14 +894,22 @@ function Editor () {
                 })
                 
                 // 使 suggest details 始终保持显示，see: https://github.com/microsoft/monaco-editor/issues/3216
-                const { value: suggest_widget } = editor.getContribution('editor.contrib.suggestController') as any
                 // monaco@0.35.0 采用 minify 过的代码，在不使用 esm bundle 的情况下，无法直接访问 suggestWidget 的属性
                 // 我们采用一种暴力的方法，直接访问 suggestWidget 的私有属性
                 // 可以从 esm 包中找到原函数 (monaco-editor/esm/vs/editor/contrib/suggest/browser/suggestWidget.js)
                 // 文件内搜索 `_setDetailsVisible` 可以搜 "expandSuggestionDocs" 这个字段
                 // 然后对比 dev 包中的代码 (monaco-editor/dev/vs/editor/editor.main.js)
                 // 也搜索 "expandSuggestionDocs" 这个字段，找到 `_setDetailsVisible` minify 之后的函数名，写在下面
-                suggest_widget.X(true)
+                let widget = editor.getContribution('editor.contrib.suggestController') as any
+                if (widget) {
+                    const { value: suggest_widget } = editor.getContribution('editor.contrib.suggestController') as any
+                    suggest_widget.X(true)
+                    
+                    // suggest_widget._persistedSize.store({
+                    //     width: 200,
+                    //     height: 256
+                    // })
+                }
                 
                 inject_css()
                 
