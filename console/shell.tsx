0e8553e9b1d4d4c6802f925d8ad1a8cf6f46f8ac
Hongfei Shen <hongfei.shen@dolphindb.com>
2023-02-27 16:27:58 +0800

    最后一部分作为表名;

--- console/shell.tsx
+++ console/shell.tsx
@@ -153,19 +153,15 @@ class ShellModel extends Model<ShellModel> {
     async load_dbs () {
         let dbs = new Map<string, DdbEntity>()
         
-        // ['dfs://数据库路径/表名', ...]
-        // 假设数据库路径不含有 /
+        // ['dfs://数据库路径(可能包含/)/表名', ...]
         // 不能使用 getClusterDFSDatabases, 因为新的数据库权限版本 (2.00.9) 之后，用户如果只有表的权限，调用 getClusterDFSDatabases 无法拿到该表对应的数据库
         const { value: fp_tables } = await ddb.call<DdbVectorStringObj>('getClusterDFSTables')
         
         let fp_dbs = new Set<string>()
         for (const fp_table of fp_tables)
-            // 找到数据库后面的斜杠位置，截取前面部分的字符串
+            // 找到数据库最后一个斜杠位置，截取前面部分的字符串作为库名
             fp_dbs.add(
-                fp_table.slice(
-                    0,
-                    fp_table.indexOf('/', 'dfs://'.length)
-                )
+                fp_table.slice(0, fp_table.lastIndexOf('/'))
             )
         
         for (const path of fp_dbs)
