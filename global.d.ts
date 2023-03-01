3b0920e7b23d0e2793039ea30cd7c12c98d29e2b
Hongfei Shen <hongfei.shen@dolphindb.com>
2023-03-01 14:07:01 +0800

    add shell model;

--- global.d.ts
+++ global.d.ts
@@ -5,6 +5,7 @@ declare module '*.ico'
 
 interface Window {
     model?: import('react-object-model').Model<any>
+    shell?: import('react-object-model').Model<any>
 }
 
 declare const BUILD_TIME: string
