c8a93115b59b14940691e0840b46d8929bd21f29
Hongfei Shen <hongfei.shen@dolphindb.com>
2023-03-16 17:04:28 +0800

    peed_table 等函数换数据节点执行 增加一些节点信息;

--- console/shell.tsx
+++ console/shell.tsx
@@ -1670,7 +1670,11 @@ class Table implements DataNode {
     
     async inspect () {
         await shell.define_peek_table()
-        let obj = await model.ddb.call('peek_table', [this.db.path.slice(0, -1), this.name])
+        let obj = await model.ddb.call(
+            'peek_table',
+            [this.db.path.slice(0, -1), this.name],
+            model.node_type === NodeType.controller ? { node: model.datanode.name, func_type: DdbFunctionType.UserDefinedFunc } : { }
+        )
         obj.name = `${this.name} (${t('前 100 行')})`
         shell.set({ result: { type: 'object', data: obj } })
     }
@@ -1682,7 +1686,8 @@ class Table implements DataNode {
             this.schema = await model.ddb.call<DdbDictObj<DdbVectorStringObj>>(
                 // 这个函数在 define_load_schema 中已定义
                 'load_schema',
-                [this.db.path, this.name]
+                [this.db.path, this.name],
+                model.node_type === NodeType.controller ? { node: model.datanode.name, func_type: DdbFunctionType.UserDefinedFunc } : { }
             )
         }
         
