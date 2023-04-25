077bc5720483f6b0eb91ae6753d47d4cf66b7617
Hongfei Shen <hongfei.shen@dolphindb.com>
2023-04-25 17:29:14 +0800

    fix format;

--- console/obj.tsx
+++ console/obj.tsx
@@ -312,12 +312,12 @@ function Vector ({
     )
     
     const nrows = info.rows === 0 ? 
-            0 
+            0
         : 
             Math.min(
-            Math.ceil(info.rows / ncols),
-            page_size / ncols
-    )
+                Math.ceil(info.rows / ncols),
+                page_size / ncols
+            )
     
     const [page_index, set_page_index] = useState(0)
     
@@ -339,8 +339,11 @@ function Vector ({
             
             const offset = page_size * page_index
             
-            // 允许在 rows 为 0 时拉一次空的 vector[0:0] 获取 obj 用来显示列名
-            if ((offset === 0 && rows === 0) || offset < rows ) {
+            if (
+                // 允许在 rows 为 0 时拉一次空的 table[0:0] 获取 obj 用来显示列名
+                rows === 0 && offset === 0 ||
+                offset < rows
+            ) {
                 const script = form === DdbForm.set ?
                     name
                 :
@@ -355,17 +358,8 @@ function Vector ({
             
                 render({ })
             }
-            
-            
         })()
     }, [obj, objref, page_index, page_size])
-    
-    
-    // if (!info.rows) 
-    //     if ([DdbType.symbol, DdbType.symbol_extended, DdbType.string].includes(objref.type))
-    //         return <>NULL</>
-    //     else
-    //         return <>0</>
     
     
     let rows = new Array<number>(nrows)
