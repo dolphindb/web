8b20c8dda1bd59c4501b16beb7088a4b0180ba52
Hongfei Shen <hongfei.shen@dolphindb.com>
2023-06-14 14:37:48 +0800

    精简主入口 index.tsx, 将组件拆分出来

---
+++ console/components/Settings.tsx
@@ -0,0 +1,99 @@
+import { default as React, useState } from 'react'
+
+import { Popover, Card, Tooltip, Button, InputNumber, message } from 'antd'
+
+import { SettingFilled, CaretUpOutlined, CaretDownOutlined } from '@ant-design/icons'
+
+import { t } from '../../i18n/index.js'
+
+import { model } from '../model.js'
+
+
+export function Settings () {
+    type DecimalsStatus = null | 'error'
+    
+    const [decimals, set_decimals] = useState<{ status: DecimalsStatus, value: number | null }>(
+        { status: null, value: model.options?.decimals ?? null }
+    )
+    
+    function confirm () {
+        if (decimals.status === null) {
+            model.set({ options: { decimals: decimals.value } })
+            message.success(t('设置成功，目前小数位数为：') + (decimals.value === null ? t('实际位数') : decimals.value))
+        } else
+            set_decimals({ status: null, value: model.options?.decimals ? model.options.decimals : null })
+    }
+    
+    function validate (text: string): { status: DecimalsStatus, value: null | number } {
+        text = text.trim()
+        if (text.length === 0) 
+            return { value: null, status: null }
+        
+        if (!/^[0-9]*$/.test(text)) 
+            return { value: null, status: 'error' }
+        
+        const num = Number(text)
+        if (Number.isNaN(num)) 
+            return { value: null, status: 'error' }
+        
+        if (num < 0 || num > 20) 
+            return { value: num, status: 'error' }
+        
+        return { value: num, status: null }
+    }
+    
+    
+    return <div className='header-settings'>
+        <Popover
+            trigger='hover'
+            placement='bottomRight'
+            zIndex={1060}
+            content={
+                <div className='header-settings-content head-bar-info'>
+                    <Card size='small' title={t('设置', { context: 'settings' })} bordered={false}>
+                        <div className='decimals-toolbar'>
+                            <span className='decimals-toolbar-input'>
+                                {t('设置小数位数: ')}
+                                <Tooltip title={t('输入应为空或介于 0 ~ 20')} placement='topLeft'>
+                                    <InputNumber
+                                        min={0}
+                                        max={20}
+                                        onStep={value => {
+                                            set_decimals(validate(value.toString()))
+                                        }}
+                                        onInput={(text: string) => {
+                                            set_decimals(validate(text))
+                                        }}
+                                        value={decimals.value}
+                                        size='small'
+                                        status={decimals.status}
+                                        onPressEnter={confirm}
+                                        controls={{ upIcon: <CaretUpOutlined />, downIcon: <CaretDownOutlined /> }}
+                                    />
+                                </Tooltip>
+                            </span>
+                            <span className='decimals-toolbar-button-group'>
+                                <Button size='small' onClick={() => {
+                                    model.set({ options: { decimals: null } })
+                                    set_decimals({ value: null, status: null })
+                                    message.success(t('重置成功，目前小数位数为：实际位数'))
+                                }}>
+                                    {t('重置')}
+                                </Button>
+                                <Button onClick={confirm} size='small' type='primary'>
+                                    {t('确定')}
+                                </Button>
+                            </span>
+                        </div>
+                    </Card>
+                </div>
+            }
+        >
+            <SettingFilled className='header-settings-icon'
+                style={{ fontSize: '20px', color: '#707070' }}
+                onMouseOver={() => {
+                    set_decimals({ value: model.options?.decimals ?? null, status: null })
+                }} />
+        </Popover>
+    </div>
+}
---
+++ console/components/Status.tsx
@@ -0,0 +1,169 @@
+import { default as React } from 'react'
+
+import { Tag, Popover, Descriptions, Card, Tooltip } from 'antd'
+
+import { t } from '../../i18n/index.js'
+
+import { model, NodeType } from '../model.js'
+
+
+const uppercase_node_types = {
+    [NodeType.data]: t('数据节点', { context: 'node_type_title' }),
+    [NodeType.controller]: t('控制节点', { context: 'node_type_title' }),
+    [NodeType.single]: t('单机节点', { context: 'node_type_title' }),
+    [NodeType.computing]: t('计算节点', { context: 'node_type_title' }),
+}
+
+const lowercase_node_types = {
+    [NodeType.data]: t('数据节点', { context: 'node_type' }),
+    [NodeType.controller]: t('控制节点', { context: 'node_type' }),
+    [NodeType.single]: t('单机节点', { context: 'node_type' }),
+    [NodeType.computing]: t('计算节点', { context: 'node_type' }),
+}
+
+export function Status () {
+    const { node_type, node_alias } = model.use(['node_type', 'node_alias'])
+    
+    return <Popover
+        placement='bottomRight'
+        zIndex={1060}
+        trigger='hover'
+        content={
+            <div className='head-bar-info'>
+                <Card
+                    size='small'
+                    title={uppercase_node_types[node_type] + t('状态', { context: 'node_type' })}
+                    bordered={false}
+                    extra={
+                        <div
+                            className='refresh'
+                            onClick={() => { model.get_cluster_perf() }}
+                        >
+                            <Tooltip title={t('刷新')} color='grey'>
+                                <SyncOutlined className='icon' />
+                            </Tooltip>
+                        </div>
+                    }
+                >
+                    <div className='status-description'>
+                        <Performance />
+                    </div>
+                </Card>
+            </div>
+        }
+    >
+        <Tag
+            className='node-info'
+            color='#f2f2f2'
+            onMouseOver={() => { model.get_cluster_perf() }}
+        >{lowercase_node_types[node_type]} {node_alias}</Tag>
+    </Popover>
+}
+
+
+function Performance () {
+    const { node } = model.use(['node'])
+    
+    if (!node)
+        return null
+        
+    return <div className='perf'>
+        <Descriptions className='table' column={2} bordered size='small' title={t('内存', { context: 'perf' })}>
+            <Descriptions.Item label={t('内存已用')}>
+                {Number(node.memoryUsed).to_fsize_str()}
+            </Descriptions.Item>
+            <Descriptions.Item label={t('内存已分配')}>
+                {Number(node.memoryAlloc).to_fsize_str()}
+            </Descriptions.Item>
+            <Descriptions.Item label={t('节点内存空间上限')}>
+                {node.maxMemSize} GB
+            </Descriptions.Item>
+        </Descriptions >
+        <Descriptions className='table' column={2} bordered size='small' title='CPU'>
+            < Descriptions.Item label={t('CPU 占用率')} >
+                {node.cpuUsage.toFixed(1)} %
+            </Descriptions.Item >
+            <Descriptions.Item label={t('CPU 平均负载')}>
+                {node.avgLoad.toFixed(1)}
+            </Descriptions.Item>
+            <Descriptions.Item label={t('worker 线程数')}>
+                {node.workerNum}
+            </Descriptions.Item>
+            <Descriptions.Item label={t('executor 线程数')}>
+                {node.executorNum}
+            </Descriptions.Item>
+        </Descriptions >
+        <Descriptions className='table' column={2} bordered size='small' title={t('磁盘')}>
+            <Descriptions.Item label={t('磁盘读速率')}>
+                {Number(node.diskReadRate).to_fsize_str()}/s
+            </Descriptions.Item>
+            <Descriptions.Item label={t('磁盘写速率')}>
+                {Number(node.diskWriteRate).to_fsize_str()}/s
+            </Descriptions.Item>
+            <Descriptions.Item label={t('前一分钟读磁盘量')}>
+                {Number(node.lastMinuteWriteVolume).to_fsize_str()}
+            </Descriptions.Item>
+            <Descriptions.Item label={t('前一分钟写磁盘量')}>
+                {Number(node.lastMinuteWriteVolume).to_fsize_str()}
+            </Descriptions.Item>
+            { node.mode !== NodeType.controller && <>
+                <Descriptions.Item label={t('磁盘剩余容量')}>
+                    {Number(node.diskFreeSpace).to_fsize_str()}
+                </Descriptions.Item>
+                <Descriptions.Item label={t('磁盘总容量')}>
+                    {Number(node.diskCapacity).to_fsize_str()}
+                </Descriptions.Item>
+                <Descriptions.Item label={t('磁盘可用空间占比')}>
+                    {(node.diskFreeSpaceRatio * 100).toFixed(2)}%
+                </Descriptions.Item>
+            </> }
+        </Descriptions >
+        <Descriptions className='table' column={2} bordered size='small' title={t('网络', { context: 'perf' })}>
+            <Descriptions.Item label={t('当前连接')}>
+                {node.connectionNum}
+            </Descriptions.Item>
+            <Descriptions.Item label={t('最大连接')}>
+                {node.maxConnections}
+            </Descriptions.Item>
+            <Descriptions.Item label={t('网络接收速率')}>
+                {Number(node.networkRecvRate).to_fsize_str()}/s
+            </Descriptions.Item>
+            <Descriptions.Item label={t('网络发送速率')}>
+                {Number(node.networkSendRate).to_fsize_str()}/s
+            </Descriptions.Item>
+            <Descriptions.Item label={t('前一分钟接收字节数')}>
+                {Number(node.lastMinuteNetworkRecv).to_fsize_str()}
+            </Descriptions.Item>
+            <Descriptions.Item label={t('前一分钟发送字节数')}>
+                {Number(node.lastMinuteNetworkSend).to_fsize_str()}
+            </Descriptions.Item>
+        </Descriptions >
+        <Descriptions className='table' column={2} bordered size='small' title={t('任务与作业')}>
+            <Descriptions.Item label={t('排队作业')}>
+                {node.queuedJobs}
+            </Descriptions.Item>
+            <Descriptions.Item label={t('运行作业')}>
+                {node.runningJobs}
+            </Descriptions.Item>
+            <Descriptions.Item label={t('排队任务')}>
+                {node.queuedTasks}
+            </Descriptions.Item>
+            <Descriptions.Item label={t('运行任务')}>
+                {node.runningTasks}
+            </Descriptions.Item>
+            {
+                Number(node.lastMsgLatency) >= 0 ? <Descriptions.Item label={t('前一批消息延时')}>
+                    {Number(node.lastMsgLatency).to_fsize_str()} s
+                </Descriptions.Item> : null
+            }
+            {
+                Number(node.cumMsgLatency) >= 0 ? <Descriptions.Item label={t('所有消息平均延时')}>
+                    {Number(node.cumMsgLatency).to_fsize_str()} s
+                </Descriptions.Item> : null
+            }
+            <Descriptions.Item label={t('作业负载')}>
+                {node.jobLoad}
+            </Descriptions.Item>
+        </Descriptions >
+    </div>
+}
