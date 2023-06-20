9ec4145f22c40889abceca2c61e47dca8907417a
Chengchen Zheng <chengchen.zheng@dolphindb.com>
2023-06-20 16:33:46 +0800

    .

--- console/overview/index.tsx
+++ console/overview/index.tsx
@@ -121,11 +121,11 @@ export function Overview () {
                     <div className='icon-area' onClick={() => { model.get_cluster_perf(true) }}><Button type='text' block icon={<Icon className='icon-refresh' component={SvgRefresh}  />}>{t('刷新')}</Button></div>
                 
                     <div className='icon-area'  onClick={() => setIsStartModalOpen(true)}>                      
-                        <Button type='text' disabled={selectedNodes.length === 0} block icon={<Icon className='icon-start' component={SvgStart}  {...selectedNodes.length === 0 ? { color: '#515151' } : { }}/>}>{t('启动')}</Button>                
+                        <Button type='text' disabled={selectedNodes.length === 0} block icon={<Icon className='icon-start' component={SvgStart}  {...selectedNodes.length === 0 ? { color: '#515151' } : { color: '#234883' }}/>}>{t('启动')}</Button>                
                     </div>
                     
                     <div className='icon-area' onClick={() => setIsStopModalOpen(true)}>
-                        <Button type='text' disabled={selectedNodes.length === 0} block icon={<Icon className='icon-stop' component={SvgStop}  {...selectedNodes.length === 0 ? { color: '#515151' } : { }} />}>{t('停止')}</Button>   
+                        <Button type='text' disabled={selectedNodes.length === 0} block icon={<Icon className='icon-stop' component={SvgStop}  {...selectedNodes.length === 0 ? { color: '#515151' } : { color: '#234883' }} />}>{t('停止')}</Button>   
                     </div>
  
                     <div className='icon-expand-area' onClick={() => expandAll()}><Button type='text' block icon={<Icon className='icon-expand' component={SvgExpand} />}>{t('全部展开')}</Button></div>
