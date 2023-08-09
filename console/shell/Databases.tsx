d759b31ed0ecff2bed4da425a1d991686a89bfa8
Xiang Guo <xiang.guo@dolphindb.com>
2023-08-09 17:16:16 +0800

    fix: 未登录状态时，无法打开交互编程

--- console/shell/Databases.tsx
+++ console/shell/Databases.tsx
@@ -135,7 +135,7 @@ export function Databases () {
                         </span>
                     </span>
                 </div>
-                {(logined || dbs.length) ?
+                {logined ?
                     (model.has_data_and_computing_nodes_alive() || node.mode === NodeType.single) ?
                         <Tree
                             className='database-tree'
