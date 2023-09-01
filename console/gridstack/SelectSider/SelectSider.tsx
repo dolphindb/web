1e08f8f07f057c711b5774223d2d1e6199c2dec6
Xiang Guo <xiang.guo@dolphindb.com>
2023-09-01 14:11:45 +0800

    feat: 选择栏新增图标及折叠功能

--- console/gridstack/SelectSider/SelectSider.tsx
+++ console/gridstack/SelectSider/SelectSider.tsx
@@ -1,11 +1,17 @@
+import { useState } from 'react'
 import { GraphTypeName } from '../graph-types.js'
+import { DoubleLeftOutlined, DoubleRightOutlined, RiseOutlined } from '@ant-design/icons'
 
 export function SelectSider () {
+    const [folding, set_folding] = useState(false)
     return <>
-        <div className='dashboard-select-sider'>
-            {Object.entries(GraphTypeName).map((graph, i) => {
-                return <div key={graph[0]} className='dashboard-graph-item grid-stack-item' data-type={graph[0]}>{graph[1]}</div>
-            })}
+        <div className={`dashboard-select-sider ${folding ? 'dashboard-select-sider-folding' : ''}`}>
+            <div className='dashboard-graph-items'>
+                {Object.entries(GraphTypeName).map((graph, i) => {
+                    return <div key={graph[0]} className='dashboard-graph-item grid-stack-item' data-type={graph[0]}><RiseOutlined /> {folding ? '' : graph[1]}</div>
+                })}
+            </div>
+            <div className='select-folder' onClick={() => set_folding(!folding)}>{folding ? <DoubleRightOutlined /> : <DoubleLeftOutlined />}</div>
         </div>
     </>
 }
