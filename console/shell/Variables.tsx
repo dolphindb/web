1b041eb81301ec567ecf57b1481daa771310b448
Hongfei Shen <hongfei.shen@dolphindb.com>
2023-09-04 11:25:54 +0800

    .

--- console/shell/Variables.tsx
+++ console/shell/Variables.tsx
@@ -382,7 +382,7 @@ class TreeDataItem implements DataNode {
         suffix?: React.ReactElement
     }) {
         const name = typeof title === 'string' ? (/^([\w\u4e00-\u9fa5]+)/.exec(title)?.[1] || title) : ''
-      
+        
         this.title = <>{typeof title === 'string' ? (
             <>
                 <div className='info'>
