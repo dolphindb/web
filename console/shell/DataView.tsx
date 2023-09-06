91c513b6075a93f02ed16d02686bbc1632366824
Sicheng Du <sicheng.du@dolphindb.com>
2023-09-06 15:44:38 +0800

    构建数据源配置sql的编辑器和预览

--- console/shell/DataView.tsx
+++ console/shell/DataView.tsx
@@ -8,8 +8,9 @@ import { model } from '../model.js'
 import { shell } from './model.js'
 
 
-export function DataView () {
-    const { result } = shell.use(['result'])
+export function DataView ({ type }: { type: string }) {
+    const result_type = type === 'dashboard' ? 'dashboard_result' : 'shell_result'
+    const result = shell.use([result_type])[result_type]
     const { options } = model.use(['options'])
     
     return <div className='dataview result embed'>{
