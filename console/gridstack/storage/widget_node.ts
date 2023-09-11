80f52d70973b8f27d1740213404257bb3bc57313
Xiang Guo <xiang.guo@dolphindb.com>
2023-09-11 16:26:41 +0800

    feat: 存储图表项配置

---
+++ console/gridstack/storage/widget_node.ts
@@ -0,0 +1,16 @@
+export type WidgetOption = {
+    id: string
+    /** 图表类型 */
+    type: string
+    /** 横轴坐标 */
+    x: number
+    /** 纵轴坐标 */
+    y: number
+    /** 宽度 */
+    w: number
+    /** 高度 */
+    h: number
+}
+
+
+export const widget_nodes: WidgetOption[] = [ ]
