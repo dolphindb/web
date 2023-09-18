2d7d8824048883ee30d9b93300dd3b5f48f0a7be
Chengchen Zheng <chengchen.zheng@dolphindb.com>
2023-09-18 13:59:39 +0800

    feat: 插值

--- console/dashboard/GraphItem/RichText/index.tsx
+++ console/dashboard/GraphItem/RichText/index.tsx
@@ -6,8 +6,29 @@ import 'react-quill/dist/quill.core.css'
 import { use_modal } from 'react-object-model/modal'
 import './index.sass'
 
+
+function replaceVariables (origin_string: string, variables: object) {
+  const regex = /{{(.*?)}}/g
+  let match = null
+  let replaced_string = origin_string
+  
+  while ((match = regex.exec(origin_string)) !== null) {
+    const full_match = match[0] // 完整的匹配，例如 "{{name}}"
+    const variable_name = match[1].trim() // 提取变量名，例如 "name"
+    const variable_value = variables[variable_name]
+    
+    // 如果找到变量值，则替换，否则保留原始字符串
+    if (variable_value !== undefined)
+        replaced_string = replaced_string.replace(full_match, variable_value)
+    
+  }
+  
+  return replaced_string
+}
+
 export function RichText () {
-  const [text, set_text] = useState('')
+  const [edit_edit, set_edit_text] = useState('')
+  const [display_text, set_display_text] = useState('')
   const { visible, open, close } = use_modal()
   const  toolbarOptions = useMemo(() => [
     ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
@@ -29,28 +50,45 @@ export function RichText () {
     ['clean']                                         // remove formatting button
   ], [ ])
   
+  const variables = {
+    name: 'rick',
+    age: 18
+  }
+  
+  const template_text = useMemo(() => 
+    replaceVariables(display_text, variables), 
+  [display_text])
+  
   return <>
     <Modal open={visible}
-           onCancel={close}
+           onCancel={() => { set_edit_text(display_text)
+                             close() }}
            okText='保存'
+           closeIcon={false}
+           onOk={() => { set_display_text(edit_edit)
+                        close() }}
            >
-        <ReactQuill theme='snow' 
-                    value={text} 
-                    onChange={set_text}
+        <ReactQuill theme='snow'
+                    value={edit_edit} 
+                    onChange={set_edit_text}
+                    
                     modules={{
                       toolbar: toolbarOptions
                     }}/>
                      
     </Modal>
-      {text === '' ?  <div className='empty-area'>
+      {display_text === '' ?  
+                      <div className='empty-area'>
                         <Button onClick={open}>
                           添加文本
                         </Button>
-                  </div> : <div className='ql-container ql-snow'>
-                            <div dangerouslySetInnerHTML={{ __html: text }}
-                                 className='display-area ql-editor'
-                                 onClick={open}/>
-                          </div>
+                      </div> 
+                          : 
+                      <div className='ql-container ql-snow'>
+                        <div dangerouslySetInnerHTML={{ __html: template_text }}
+                             className='display-area ql-editor'
+                             onClick={open}/>
+                      </div>
       }
     </>
 }
