72f6b25f590079b232a32b5bed0c1695e0221c78
Yuke Ying <yuke.ying@dolphindb.com>
2023-09-20 10:40:03 +0800

    fix: 修改列名

--- console/dashboard/GraphItem/GraphItem.tsx
+++ console/dashboard/GraphItem/GraphItem.tsx
@@ -8,25 +8,24 @@ import { type Widget } from '../model.js'
 import { graph_config } from '../graph-config.js'
 
 import ChartSvg from '../../icons/chartIcon.svg'
+import { get_data_source_node } from '../storage/date-source-node.js'
 
 import './index.scss'
+
+const GraphComponent = ({ widget }: { widget: Widget }) => {
+    const data_source_node = get_data_source_node(widget.source_id)
+    
+    const { data = [ ] } = data_source_node.use(['data'])
+    console.log(data, 'data')
+    
+    const Component = useMemo(() => graph_config[widget.type].component, [widget.type])
+    
+    return <Component data_source={data} widget={widget} />
+}
 
 
 export function GraphItem  ({ widget }: { widget: Widget }) {
     const { widget: current } = dashboard.use(['widget'])
-    const [data_source, set_data_source] = useState([ ])
-    
-    useEffect(() => {
-        if (!widget.update_graph)
-            dashboard.update_widget({
-                ...widget,
-                update_graph: data => { 
-                    set_data_source(data)
-                }
-            })
-    }, [ ])
-    
-    const GraphComponent = useMemo(() => graph_config[widget.type].component, [widget.type])
     
     // grid-stack-item-content 类名不能删除，gridstack 库是通过该类名去获取改 DOM 实现拖动
     // 根据 id 判断当前选中的节点，防止因为对象引用地址不同导致的判断错误
@@ -36,7 +35,7 @@ export function GraphItem  ({ widget }: { widget: Widget }) {
         </div>
         {
             widget.source_id && widget.config ? 
-                <GraphComponent data_source={data_source} widget={widget} />
+                <GraphComponent widget={widget} />
             :
                 <div className='graph-content'>
                     <div className='graph-title'>{WidgetType[widget.type]}</div>
