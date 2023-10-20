9ef9ca50d063f94528446894b9e446c55633d375
Yuke Ying <yuke.ying@dolphindb.com>
2023-10-20 17:09:53 +0800

    feat: 修改

--- console/components/DdbHeader.tsx
+++ console/components/DdbHeader.tsx
@@ -15,7 +15,8 @@ import { Settings } from './Settings.js'
 import { CompileAndRefresh } from './CompileAndRefresh.js'
 
 import SvgArrowDown from './icons/arrow.down.icon.svg'
-import { HostChangeBtn } from './HostChangeBtn.js'
+import { HostSelect } from './HostSelect.js'
+
 
 
 export function DdbHeader () {
@@ -29,7 +30,7 @@ export function DdbHeader () {
     
     return <>
         <img className='logo' src='./ddb.svg' />
-        {(model.dev || model.cdn) && <HostChangeBtn />}
+        {(model.dev || model.cdn) && <HostSelect />}
         { model.dev && <CompileAndRefresh /> }
         
         <div className='padding' />
