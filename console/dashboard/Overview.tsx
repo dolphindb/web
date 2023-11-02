990d27a1f1d686afeebe4a31a6687b49978303b5
Chengchen Zheng <chengchen.zheng@dolphindb.com>
2023-11-02 16:11:23 +0800

    fix: delete msg;

--- console/dashboard/Overview.tsx
+++ console/dashboard/Overview.tsx
@@ -166,6 +166,7 @@ export function Overview () {
                         try {
                             await dashboard.delete_dashboard_configs(selected_dashboard_ids, false)
                             set_selected_dashboard_ids([ ])
+                            model.message.success(t('删除成功'))
                             deletor.close()
                         } catch (error) {
                             model.show_error({ error })
