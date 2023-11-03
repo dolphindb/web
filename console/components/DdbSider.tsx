c7dac612f9001ba6b8e293f8609934cd3a809d4d
Hongfei Shen <hongfei.shen@dolphindb.com>
2023-11-03 11:30:43 +0800

    .

--- console/components/DdbSider.tsx
+++ console/components/DdbSider.tsx
@@ -121,11 +121,11 @@ export function DdbSider () {
                     label: <Link target='_blank' href={factor_href}>{t('因子平台')}</Link>
                 }] : [ ],
                 ... model.dev || model.cdn ? [
-                    {
-                        key: 'dashboard',
-                        icon: <MenuIcon view='dashboard' />,
-                        label: t('数据面板'),
-                    },
+                    // {
+                    //     key: 'dashboard',
+                    //     icon: <MenuIcon view='dashboard' />,
+                    //     label: t('数据面板'),
+                    // },
                     {
                         key: 'test',
                         icon: <ExperimentOutlined className='icon-menu' />,
