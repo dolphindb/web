87fd162db1f8f03e2680ce77dfccb8390fa875ed
Chengchen Zheng <chengchen.zheng@dolphindb.com>
2023-12-14 16:50:13 +0800

    fix: format size

--- console/config/ControllerConfig.tsx
+++ console/config/ControllerConfig.tsx
@@ -14,16 +14,16 @@ type DataSourceType = {
 }
 
 export function ControllerConfig () {
-    // const [configs, set_configs] = useState<string[]>(['lanCluster=0', 'maxPubConnections=64', 'logLevel=DEBUG', 'maxPartitionNumPerQuery=100000000', 'chunkCacheEngineMemSize=4', 'maxMemSize=24', 'newValuePartitionPolicy=add', 'workerNum=4', 'localExecutors=3', 'memoryReleaseRate=10', 'remoteExecutors=5', 'updateCidVersionsKeepTime=60', 'streamingHAMode=raft', 'streamingRaftGroups=11:P1_node1:P2_node1:P3_node1,12:P1_node2:P2_node2:P3_node2', 'streamingHAPreVote=false', 'persistenceDir=/home/yzou/Desktop/DolphinDB/server200_HA/persistence/<ALIAS>', 'P1_node1.subPort=20101', 'P1_node2.subPort=20102', 'P1_node3.subPort=20103', 'P2_node1.subPort=20201', 'P2_node2.subPort=20202', 'P3_node1.subPort=20301', 'P3_node2.subPort=20302', 'P1_node4.subPort=20105', 'TSDBCacheEngineSize=6', 'OLAPCacheEngineSize=6', 'dataSync=1', 'enableChunkGranularityConfig=true', 'regularArrayMemoryLimit=512', 'maxLogSize=2048', 'enableCoreDump=1', 'dfsMetaDir=/home/yzou/Desktop/DolphinDB/server200_HA', 'dfsRebalanceConcurrency=5', 'dfsChunkNodeHeartBeatTimeout=1', 'clusterReplicationSlaveNum=2', 'strictPermissionMode=false', 'moduleDir=/home/yzou/Desktop/DolphinDB/server200_HA/modules', 'preloadModules=example::tt', 'clusterReplicationExecutionUsername=admin', 'clusterReplicationExecutionUsername=admin', 'P9_node8.maxLogSize=1024'])
+    const [configs, set_configs] = useState<string[]>([ ])
     const [editableKeys, setEditableRowKeys] = useState<React.Key[]>([ ])
     const actionRef = useRef<ActionType>()
-    const configs = ['lanCluster=0', 'maxPubConnections=64', 'logLevel=DEBUG', 'maxPartitionNumPerQuery=100000000', 'chunkCacheEngineMemSize=4', 'maxMemSize=24', 'newValuePartitionPolicy=add', 'workerNum=4', 'localExecutors=3', 'memoryReleaseRate=10', 'remoteExecutors=5', 'updateCidVersionsKeepTime=60', 'streamingHAMode=raft', 'streamingRaftGroups=11:P1_node1:P2_node1:P3_node1,12:P1_node2:P2_node2:P3_node2', 'streamingHAPreVote=false', 'persistenceDir=/home/yzou/Desktop/DolphinDB/server200_HA/persistence/<ALIAS>', 'P1_node1.subPort=20101', 'P1_node2.subPort=20102', 'P1_node3.subPort=20103', 'P2_node1.subPort=20201', 'P2_node2.subPort=20202', 'P3_node1.subPort=20301', 'P3_node2.subPort=20302', 'P1_node4.subPort=20105', 'TSDBCacheEngineSize=6', 'OLAPCacheEngineSize=6', 'dataSync=1', 'enableChunkGranularityConfig=true', 'regularArrayMemoryLimit=512', 'maxLogSize=2048', 'enableCoreDump=1', 'dfsMetaDir=/home/yzou/Desktop/DolphinDB/server200_HA', 'dfsRebalanceConcurrency=5', 'dfsChunkNodeHeartBeatTimeout=1', 'clusterReplicationSlaveNum=2', 'strictPermissionMode=false', 'moduleDir=/home/yzou/Desktop/DolphinDB/server200_HA/modules', 'preloadModules=example::tt', 'clusterReplicationExecutionUsername=admin', 'clusterReplicationExecutionUsername=admin', 'P9_node8.maxLogSize=1024']
-    // useEffect(() => {
-    //     model.execute(async () => {
-    //         const { value } = await config.load_controller_configs()
-    //         set_configs(value as [])
-    //     })
-    // }, [ ])
+    // const configs = [ ]
+    useEffect(() => {
+        model.execute(async () => {
+            const { value } = await config.load_controller_configs()
+            set_configs(value as [])
+        })
+    }, [ ])
     
     // console.log('configs', configs)
     const cols: ProColumns<DataSourceType>[] = useMemo(() => ([
@@ -69,43 +69,43 @@ export function ControllerConfig () {
           },
     ]), [ ])
     return <EditableProTable 
-            rowKey='id'
-            actionRef={actionRef}
-            columns={cols}
-            value={configs.map(cfg => {
-                const [name, value] = cfg.split('=')
-                return {
-                    id: cfg,
-                    name,
-                    value,
-                    
-                }
-            })}
-            toolBarRender={() => [
-                <Button
-                    type='primary'
-                    className='mr-btn'
-                    onClick={() => {
-                        actionRef.current?.addEditRecord?.({
-                        id: (Math.random() * 1000000).toFixed(0),
-                        title: '新的一行',
-                        })
-                    }}
-                    icon={<PlusOutlined />}
-                >
-                    {t('新增')}
-                </Button>,
-                <Button
-                    type='primary'
-                    className='mr-btn'
-                    icon={<ReloadOutlined />}
+                rowKey='id'
+                actionRef={actionRef}
+                columns={cols}
+                value={configs.map(cfg => {
+                    const [name, value] = cfg.split('=')
+                    return {
+                        id: cfg,
+                        name,
+                        value,
+                        
+                    }
+                })}
+                toolBarRender={() => [
+                    <Button
+                        type='primary'
+                        className='mr-btn'
+                        onClick={() => {
+                            actionRef.current?.addEditRecord?.({
+                            id: (Math.random() * 1000000).toFixed(0),
+                            title: '新的一行',
+                            })
+                        }}
+                        icon={<PlusOutlined />}
                     >
-                        {t('刷新')}
-                </Button>,
-                <Input
-                    placeholder={t('请输入想要查找的配置项')}
-                    prefix={<SearchOutlined/>}
-                />
+                        {t('新增')}
+                    </Button>,
+                    <Button
+                        type='primary'
+                        className='mr-btn'
+                        icon={<ReloadOutlined />}
+                        >
+                            {t('刷新')}
+                    </Button>,
+                    <Input
+                        placeholder={t('请输入想要查找的配置项')}
+                        prefix={<SearchOutlined/>}
+                    />
             ]
                 
             }
