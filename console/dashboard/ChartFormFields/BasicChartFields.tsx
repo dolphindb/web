9dd27f8d0cedfa0be51399400e10851ff688343a
Yuke Ying <yuke.ying@dolphindb.com>
2023-12-21 09:25:21 +0800

    feat: 增加时间格式化类型

--- console/dashboard/ChartFormFields/BasicChartFields.tsx
+++ console/dashboard/ChartFormFields/BasicChartFields.tsx
@@ -89,7 +89,7 @@ export function AxisItem ({ name_path, col_names = [ ], list_name, initial_value
                                 <Select options={convert_list_to_options(col_names)} allowClear/>
                             </Form.Item>
                             <Form.Item name={concat_name_path(name_path, 'time_format')} label={t('时间格式化')}>
-                                <Select options={format_time_options.slice(3)}/>
+                                <Select options={format_time_options.slice(4)}/>
                             </Form.Item>
                             <FormDependencies dependencies={[concat_name_path(list_name, name_path, 'time_format')]}>
                                 {value => { 
