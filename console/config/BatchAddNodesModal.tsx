a4f306d2082fe91e490d75519c200e91786054ec
Chengchen Zheng <chengchen.zheng@dolphindb.com>
2023-12-22 13:33:31 +0800

    fix: remove checkbox before conntroller nodes in overview

--- console/config/BatchAddNodesModal.tsx
+++ console/config/BatchAddNodesModal.tsx
@@ -1,7 +1,12 @@
-import NiceModal from '@ebay/nice-modal-react'
+import NiceModal, { useModal } from '@ebay/nice-modal-react'
 import { Modal } from 'antd'
 import { t } from '../../i18n/index.js'
 
 export const BatchAddNodesModal = NiceModal.create(() => {
-    return <Modal title={t('批量新增节点')}>d</Modal>
+    const modal = useModal()
+    return <Modal 
+            open={modal.visible}
+            onCancel={modal.hide}
+            afterClose={modal.remove}
+            title={t('批量新增节点')}>d</Modal>
 })
