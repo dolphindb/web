8eb932d9b7d8a7c95f0a94b0e76f7690a69c4411
Hongfei Shen <hongfei.shen@dolphindb.com>
2024-03-13 09:58:45 +0800

    .

--- console/index.tsx
+++ console/index.tsx
@@ -75,6 +75,7 @@ function MainLayout () {
                 error.shown = true
                 
                 // 忽略 monaco editor 的错误
+                // https://github.com/microsoft/monaco-editor/issues/4325
                 if (error.message.includes('getModifierState is not a function'))
                     return
                 
