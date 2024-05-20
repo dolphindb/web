409d151f90bd57ea408db18737c05cfc10c58c0c
Hongfei Shen <hongfei.shen@dolphindb.com>
2024-05-20 14:51:46 +0800

    with resovlers;

--- console/model.ts
+++ console/model.ts
@@ -906,6 +906,17 @@ export class DdbModel extends Model<DdbModel> {
 }
 
 
+if (!Promise.withResolvers) {
+    Promise.withResolvers = function PromiseWithResolvers () {
+        let resolve: any, reject: any
+        let promise = new Promise((_resolve, _reject) => {
+            resolve = _resolve
+            reject = _reject
+        })
+        return { promise, resolve, reject }
+    } as any
+}
+
 if (!Array.prototype.toReversed)
     Object.defineProperty(Array.prototype, 'toReversed', {
         configurable: true,
