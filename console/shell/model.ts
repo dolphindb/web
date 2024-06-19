19c2baec4a6f9965e2591020a975ef91cd98a854
Hongfei Shen <hongfei.shen@dolphindb.com>
2024-06-19 10:01:56 +0800

    .

--- console/shell/model.ts
+++ console/shell/model.ts
@@ -645,21 +645,22 @@ class ShellModel extends Model<ShellModel> {
     
     
     async define_get_csv_content () {
-        if (this.get_csv_content_defined)
-            return
-        await model.ddb.eval(
-            'def get_csv_content (name_or_obj, start, end) {\n' +
-            '    type = typestr name_or_obj\n' +
-            "    if (type =='CHAR' || type =='STRING')\n" +
-            '        obj = objByName(name_or_obj)\n' +
-            '    else\n' +
-            '        obj = name_or_obj\n' +
-            '        \n' +
-            '    table_size = size obj\n' +
-            "    return generateTextFromTable(obj, start, end - start + 1, 0, ',', true)\n" +
-            '}\n'
-        )
-        this.set({ get_csv_content_defined: true })
+        if (!this.get_csv_content_defined) {
+            await model.ddb.eval(
+                'def get_csv_content (name_or_obj, start, end) {\n' +
+                '    type = typestr name_or_obj\n' +
+                "    if (type =='CHAR' || type =='STRING')\n" +
+                '        obj = objByName(name_or_obj)\n' +
+                '    else\n' +
+                '        obj = name_or_obj\n' +
+                '        \n' +
+                '    table_size = size obj\n' +
+                "    return generateTextFromTable(obj, start, end - start + 1, 0, ',', true)\n" +
+                '}\n'
+            )
+            
+            this.set({ get_csv_content_defined: true })
+        }
     }
 }
 
