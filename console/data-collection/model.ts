bdc36cb7894b2a1444e02c6fbd006b504764c56f
Yuke Ying <yuke.ying@dolphindb.com>
2024-06-24 17:21:25 +0800

    feat: save

--- console/data-collection/model.ts
+++ console/data-collection/model.ts
@@ -14,9 +14,8 @@ export class DcpModel extends Model<DcpModel> {
     async init () {
         await model.ddb.eval(installMqttCode)
         this.set({ func_inited: true })
-        const { value: is_inited } = await model.ddb.eval('existsDatabase("dfs://dataAcquisition")')
-        console.log(typeof is_inited, 'is_inited')
-        this.set({ database_inited: is_inited ? 'inited' : 'not_inited' } )        
+        const res = await model.ddb.eval('existsDatabase("dfs://dataAcquisition")')
+        this.set({ database_inited: res.value ? 'inited' : 'not_inited' } )        
     }
     
     async init_database () {
