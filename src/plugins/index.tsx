f819f0a3a0fbd8d22cc1c5821798403c3f08457c
Hongfei Shen <hongfei.shen@dolphindb.com>
2024-07-11 18:02:10 +0800

    .

--- src/plugins/index.tsx
+++ src/plugins/index.tsx
@@ -1,13 +1,16 @@
 import './index.sass'
 
-import { useEffect, useState } from 'react'
-import { Button, Input, Table } from 'antd'
+import { useEffect, useState, type ReactElement, type ReactNode } from 'react'
+import { Button, Input, Table, Typography } from 'antd'
 import { ReloadOutlined } from '@ant-design/icons'
 
 import { model } from '@/model.js'
 
 import script from './index.dos'
 import { t } from '@i18n/index.js'
+
+
+const { Text } = Typography
 
 
 export function Plugins () {
@@ -50,17 +53,61 @@ export function Plugins () {
                 },
                 {
                     title: '集群已安装的最低版本', 
-                    dataIndex: 'least_version',
-                    width: 200,
+                    width: 400,
+                    render: (_, { least_version }) => {
+                        const match = least_version.startsWith(
+                            model.version.split('.').slice(0, 3).join('.')  // 去掉 patch 部分
+                        )
+                        
+                        return <Text type={ match ? undefined : 'danger'}>{least_version}{ !match && ' (与数据库版本不一致，无法加载)' }</Text>
+                    }
                 },
                 {
                     title: '已部署节点',
-                    render: (_, { nodes }) =>
-                        nodes.map(({ node }) => node).join(', ')
+                    render: (_, { nodes, least_version }, j) => {
+                        let all_match = true
+                        
+                        const elements = nodes.map(({ node, version }, index) => {
+                            // local
+                            // if (index === 0 && j % 2 === 0)
+                            //     version = '2.00.12.1'
+                            
+                            const match = version === least_version
+                            if (!match)
+                                all_match = false
+                            
+                            return match
+                                ? node
+                                : <Text key={node} className='danger-node' type={ match ? undefined : 'danger'}>
+                                    {node} ({version})
+                                </Text>
+                        })
+                        
+                        return <>
+                            { join_elements(elements, <span>{', '}</span>) }
+                            { !all_match && <Text type='danger'> (不同节点插件版本不一致，需要同步)</Text> }
+                        </>
+                    }
                 }
             ]}
         />
     </>
+}
+
+
+function join_elements (elements: ReactNode[], seperator: ReactElement) {
+    const nelements = elements.length
+    let results = new Array(nelements * 2 - 1)
+    
+    for (let i = 0;  i < nelements;  i++) {
+        results[i * 2] = elements[i]
+        if (i < nelements - 1) {
+            const key = i * 2 + 1
+            results[key] = { ...seperator, key }
+        }
+    }
+    
+    return results
 }
 
 
