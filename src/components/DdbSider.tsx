c4b8b6fc2a349e921806be6a841df169c283a87b
Chengchen Zheng <chengchen.zheng@dolphindb.com>
2024-07-15 17:11:51 +0800

    fix;

--- src/components/DdbSider.tsx
+++ src/components/DdbSider.tsx
@@ -113,11 +113,13 @@ export function DdbSider () {
             }}
             inlineIndent={10}
             items={[
-                {
-                    key: 'overview',
-                    icon: <MenuIcon view='overview' />,
-                    label: node_type === NodeType.single ? t('单机总览') : t('集群总览'),
-                },
+                ...node_type === NodeType.controller || node_type === NodeType.single ? [
+                    {
+                        key: 'overview',
+                        icon: <MenuIcon view='overview' />,
+                        label: node_type === NodeType.single ? t('单机总览') : t('集群总览'),
+                    },
+                ] : [ ],
                 ...admin && node_type === NodeType.controller ? [{
                     key: 'config',
                     icon: <MenuIcon view='config'/>,
