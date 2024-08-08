acb01a561f9de72eb98695e2bc5dbb1ffc00efa2
Tianchen Fu <tianchen.fu@dolphindb.com>
2024-08-08 18:05:29 +0800

    feat: init compute group config edit

--- src/config/ComputeGroupConfig.tsx
+++ src/config/ComputeGroupConfig.tsx
@@ -1,19 +1,25 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 import { t } from '@i18n/index.js'
+
+import { Button, Popconfirm } from 'antd'
+
+import { EditableProTable, type ActionType, type ProColumns } from '@ant-design/pro-components'
 
 import { config } from './model.js'
 
 import { model } from '@/model.js'
 
-
 import './index.sass'
 
 export function ComputeGroupConfig () {
 
-    const { nodes_configs } = config.use(['nodes_configs'])
+    const { nodes_configs: nodesConfigRaw } = config.use(['nodes_configs']) // 获取原始的 nodes_configs 字符串数组
     const { nodes, node_type, logined } = model.use(['nodes', 'node_type', 'logined'])
     const [current_compute_group, set_current_compute_group] = useState<string>('')
+    
+    // 将 nodes_configs 字符串数组转换为 NodesConfig 对象数组
+    const nodes_configs = Array.from(nodesConfigRaw.values())
     
     const compute_groups = new Map()
     nodes.forEach(config => {
@@ -48,9 +54,80 @@ export function ComputeGroupConfig () {
     })
     
     
+    // 筛选配置项
+    const filtered_configs = nodes_configs.filter(config => {
+        return config.qualifier.startsWith(`${current_compute_group}%`)
+    })
+    
+    
+    const actionRef = useRef<ActionType>()
+    
+    const columns: ProColumns<any>[] = [
+        {
+            title: t('配置项'),
+            dataIndex: 'key',
+            key: 'qualifier',
+            width: 400,
+        },
+        {
+            title: t('值'),
+            dataIndex: 'value',
+            key: 'value',
+        },
+        {
+            title: t('操作'),
+            valueType: 'option',
+            key: 'actions',
+            width: 240,
+            render: (text, record, _, action) => [
+                <Button
+                    type='link'
+                    key='editable'
+                    className='mr-btn'
+                    onClick={() => {
+                        action?.startEditable?.(record.id)
+                    }}
+                >
+                    {t('编辑')}
+                </Button>,
+                <Popconfirm
+                    title={t('确认删除此配置项？')}
+                    key='delete'
+                    onConfirm={async () => {
+                    }}
+                >
+                    <Button type='link'>
+                        {t('删除')}
+                    </Button>
+                </Popconfirm>
+            ],
+        },
+    ]
+    
     return <div className='config compute-group'>
         <div className='select' >
             {select_items}
         </div>
+        <EditableProTable
+            rowKey='key'
+            actionRef={actionRef}
+            columns={columns}
+            dataSource={filtered_configs}
+            request={async () => {
+                return {
+                    data: filtered_configs,
+                    success: true,
+                    total: filtered_configs.length
+                }
+            }}
+            editable={{
+                type: 'single',
+                onSave: async (rowKey, data, row) => {
+                },
+                onDelete: async (key, row) => {
+                },
+                deletePopconfirmMessage: t('确认删除此配置项？'),
+            }}
+        />
     </div>
 }
