0e9694402272f4a0ca2fa41861f1169d86760d09
Hongfei Shen <hongfei.shen@dolphindb.com>
2024-08-16 17:16:09 +0800

    .

--- src/model.ts
+++ src/model.ts
@@ -236,7 +236,7 @@ export class DdbModel extends Model<DdbModel> {
             config.set_config('oauthClientId', 'Ov23liZJ5nXZvunhpJLI')
             config.set_config('oauthClientSecret', '3c289d4ab4cee18f834d66a94b38c736fc52e40a')
             
-            config.delete_config('oauthRedirectUri')
+            // config.delete_config('oauthRedirectUri')
             config.set_config('oauthRedirectUri', 'http://test.dolphindb.cn/web/?hostname=192.168.0.200&port=20023'.quote())
             
             config.set_config('oauthTokenUri', 'https://github.com/login/oauth/access_token')
