28b6ae7d24a36ed1a34068d43de50818833f301b
Chengchen Zheng <chengchen.zheng@dolphindb.com>
2024-08-26 16:08:01 +0800

    feat: inpection form layout

--- src/inspection/inspectionForm.tsx
+++ src/inspection/inspectionForm.tsx
@@ -1,21 +1,42 @@
 import { t } from "@i18n/index.ts"
-import { Table } from "antd"
+import { Form, Input, InputNumber, Select, Table, TimePicker } from "antd"
+import dayjs from "dayjs"
 import { useMemo } from "react"
+
+const inspectionFrequencyOptions = [
+    {
+        label: '每日',
+        value: 'daily',
+    },
+    {
+        label: '每周',
+        value: 'weekly',
+    },
+    {
+        label: '每月',
+        value: 'monthly',
+    },
+]
 
 export function InspectionForm () {
     const cols = useMemo(() => [ 
         {
-            title: '名称',
+            title: t('名称'),
             dataIndex: 'key',
             key: 'key',
         },
         {
-            title: '描述',
+            title: t('描述'),
             dataIndex: 'description',
             key: 'description',
         },
         {
-            title: '巡检节点',
+            title: t('巡检节点'),
+            dataIndex: 'subbmiter',
+            key: 'subbmiter',
+        },
+        {
+            title: t('脚本内容'),
             dataIndex: 'subbmiter',
             key: 'subbmiter',
         },
@@ -25,5 +46,40 @@ export function InspectionForm () {
     return <div >
         <h3>{t('指标列表')}</h3>
         <Table rowSelection={{ type: 'checkbox' }} dataSource={[ ]} columns={cols} />
+        
+        <h3>{t('巡检周期')}</h3>
+        <Form layout="inline">
+            <Form.Item label={t('巡检频率')} name='frequency'>
+                <Select options={inspectionFrequencyOptions} defaultValue={inspectionFrequencyOptions[0].value}/>
+            </Form.Item>
+            
+            <Form.Item 
+                label={t('巡检开始日期')} 
+                dependencies={['frequency']} 
+                shouldUpdate={(prevValues, curValues) => prevValues.frequency !== curValues.frequency}>
+                {
+                    ({getFieldValue })=>{
+                        const frequency = getFieldValue('frequency')
+                        return <Form.Item name='date'>
+                                <InputNumber 
+                                    addonBefore={t('第')} 
+                                    addonAfter={t('天')} 
+                                    defaultValue={1} 
+                                    max={frequency === 'monthly' ? dayjs().daysInMonth() : frequency === 'weekly' ? 7 : 1} 
+                                    min={1}/> 
+                        </Form.Item>
+                    }
+                }
+            </Form.Item>
+            
+            <Form.Item label={t('巡检时间')}  name='time'>
+                <TimePicker />
+            </Form.Item>
+        </Form>
+        
+        <h3>{t('巡检计划描述')}</h3>
+        <Input/>
     </div>
 }
+
+
