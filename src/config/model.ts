c78142605e1e7396e61182cbfb96129d1c18b12d
Hongfei Shen <hongfei.shen@dolphindb.com>
2024-09-25 14:14:44 +0800

    .

--- src/config/model.ts
+++ src/config/model.ts
@@ -110,11 +110,14 @@ class ConfigModel extends Model<ConfigModel> {
         
         await this.invoke(
             'saveClusterNodesConfigs', 
-            [[...iterator_map(this.nodes_configs.entries(), ([key, config]) => {
-                new_nodes_configs.set(key, config)
-                const { value } = config
-                return `${key}=${value}`
-            })]])
+            [[...iterator_map(
+                this.nodes_configs.entries(), 
+                ([key, config]) => {
+                    new_nodes_configs.set(key, config)
+                    const { value } = config
+                    return `${key}=${value}`
+                })
+            ]])
         
         await this.invoke('reloadClusterConfig')
         
