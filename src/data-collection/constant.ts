1f946d0e31850f62313fb1e5c203ad689527df49
Yuke Ying <yuke.ying@dolphindb.com>
2024-10-09 18:01:25 +0800

    feat: 文案调整

--- src/data-collection/constant.ts
+++ src/data-collection/constant.ts
@@ -19,7 +19,7 @@ export const template_code = language === 'en'
     ? '    def mqttTemplate(outTableName,args,topic,msg){\n' +
     '        /*\n' +
     '            outTableName: The name of the table where data will be stored\n' +
-    '            args: Custom arguments (can be defined as needed))\n' +
+    '            args: Custom arguments (can be defined as needed)\n' +
     '            topic: The name of the topic to which MQTT data is subscribed\n' +
     '            msg: The received message\n' +
     '        */\n' +
@@ -28,9 +28,9 @@ export const template_code = language === 'en'
     '     def kafkaTemplate(args,msg,key,topic){\n' +
     '        /*\n' +
     '            args: Custom arguments (can be defined as needed) \n' +
-    '            topic: The name of the topic to which Kafka data is subscribed \n' +
     '            msg: The received message \n' +
     '            key: The key corresponding to the received message \n' +
+    '            topic: The name of the topic to which Kafka data is subscribed \n' +
     '            @return: A table \n' +
     '        */\n' +
     '    }\n' 
@@ -46,9 +46,9 @@ export const template_code = language === 'en'
     '     def kafkaTemplate(args,msg,key,topic){\n' +
     '        /*\n' +
     '            args 为自定义参数（可任意自定义）\n' +
-    '            topic 为订阅kafka数据的topic名称\n' +
     '            msg 为接收到的消息\n' +
     '            key 为接收到的消息所对应的key值\n' +
+    '            topic 为订阅kafka数据的topic名称\n' +
     '            @return 需要为一个表\n' +
     '        */\n' +
     '    }\n'
