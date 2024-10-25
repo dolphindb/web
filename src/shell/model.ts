bcf03c620a08ef30d71caa045c3be5ad7d6dce7a
Tianchen Fu <tianchen.fu@dolphindb.com>
2024-10-25 14:27:59 +0800

    fix: save undefined code error

--- src/shell/model.ts
+++ src/shell/model.ts
@@ -315,15 +315,17 @@ class ShellModel extends Model<ShellModel> {
     
     
     save (code = this.editor?.getValue()) {
+        if (code === undefined) 
+            throw new Error('saving undefind code')
+        
         if (this.itab > -1) {
             let tab = this.tabs.find(t => t.index === this.itab)
-            if (code || code === '')
+            if (tab)
                 tab.code = code
             this.set({ tabs: [...this.tabs] })
             localStorage.setItem(`${storage_keys.code}.${this.itab}`, JSON.stringify(tab))
         } else
-            if (code || code === '') // 空字符串也要存
-                localStorage.setItem(storage_keys.code, code)
+            localStorage.setItem(storage_keys.code, code)
     }
     
     
