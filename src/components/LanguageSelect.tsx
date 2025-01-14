facc21f51bde71cd20588df98da1d5767daa501b
Chengchen Zheng <chengchen.zheng@dolphindb.com>
2025-01-14 17:40:09 +0800

    language select

--- src/components/LanguageSelect.tsx
+++ src/components/LanguageSelect.tsx
@@ -2,47 +2,54 @@ import { Dropdown, type  MenuProps } from 'antd'
 
 import { t } from '@i18n/index.ts'
 
+import { useState } from 'react'
+
 import SvgI18n from '@/icons/i18n.icon.svg'
-import { storage_keys } from '@/model.ts'
+import { model, storage_keys } from '@/model.ts'
 
 
 export function LanguageSelect () {
+    
+    const [selected, set_selected] = useState(localStorage.getItem(storage_keys.language) || 'auto')
+    
     function handleLanguageChange (lang: string) {
-        const searchParams = new URLSearchParams(location.search)
         if (lang === 'auto') {
             localStorage.removeItem(storage_keys.language)
-            searchParams.delete('language')
+            model.set_query('language', null)
         } else {
-            searchParams.set('language', lang)
+            model.set_query('language', lang)
             localStorage.setItem(storage_keys.language, lang)
         }
-        location.search = searchParams.toString()
+        location.reload()
     }
     
     const items: MenuProps['items'] = [
         {
             key: 'zh',
             label: '中文',
-            onClick: () => { handleLanguageChange('zh') }
         },
         {
             key: 'en',
             label: 'English',
-            onClick: () => { handleLanguageChange('en') }
         },
         {
             key: 'auto',
             label: t('自动'),
-            onClick: () => { handleLanguageChange('auto') }
         }
     ]
     
     return <Dropdown
             menu={{
                 items,
-                onChange () {
-                    
-                }
+                onClick: ({ key }) => {
+                    handleLanguageChange(key)
+                    set_selected(key)
+                },
+                onSelect: ({ key }) => {
+                    set_selected(key)
+                },
+                selectedKeys: [selected],
+                
             }}
             placement='bottomRight'
             trigger={['hover', 'click']}
