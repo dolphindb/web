e9be940d6fef84c4bd6d8b3be028830bacd82e29
Chengchen Zheng <chengchen.zheng@dolphindb.com>
2025-01-16 17:05:17 +0800

    .

--- src/access/model.ts
+++ src/access/model.ts
@@ -130,6 +130,8 @@ class AccessModel extends Model<AccessModel> {
     
     // final 属性代表是否获取用户最终权限，只有在用户查看权限界面需要 final = true
     async get_user_access (users: string[], final: boolean = false) {
+        if (!users || !users.length)
+            return [ ]
         return (await model.ddb.invoke('getUserAccess', [...final ? [users, true] : [users]]))
             .data
     }
@@ -141,6 +143,8 @@ class AccessModel extends Model<AccessModel> {
     
     
     async get_group_access (groups: string[]) {
+        if (!groups || !groups.length)
+            return [ ]
         return (await model.ddb.invoke('getGroupAccess', [ groups ])).data
     }
     
