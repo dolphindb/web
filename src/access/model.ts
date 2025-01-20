ff75fdfee0524144fa54280a968a37a20219bf18
Hongfei Shen <hongfei.shen@dolphindb.com>
2025-01-20 14:08:22 +0800

    .

--- src/access/model.ts
+++ src/access/model.ts
@@ -78,10 +78,10 @@ class AccessModel extends Model<AccessModel> {
     
     async get_catelog_with_schemas_v2 () {
         return [{ 
-                name: DATABASES_WITHOUT_CATALOG, 
-                schemas: ((await this.get_databases_with_tables()) as Database[]).
-                            map(db => ({ schema: db, dbUrl: db, tables: [ ] })) 
-                }]
+            name: DATABASES_WITHOUT_CATALOG, 
+            schemas: ((await this.get_databases_with_tables()) as Database[]).
+                        map(db => ({ schema: db, dbUrl: db, tables: [ ] })) 
+        }]
     }
     
     async get_databases_with_tables (has_schema: boolean = false) {
