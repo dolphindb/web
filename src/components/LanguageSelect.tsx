e4c9b3ef2dc73eb7364187f9dd1d9cadfbe20be6
Yuke Ying <yuke.ying@dolphindb.com>
2025-02-12 11:53:06 +0800

    feat: i18n icon

--- src/components/LanguageSelect.tsx
+++ src/components/LanguageSelect.tsx
@@ -4,6 +4,8 @@ import { t } from '@i18n/index.ts'
 
 import { useState } from 'react'
 
+import { GlobalOutlined, TranslationOutlined } from '@ant-design/icons'
+
 import SvgI18n from '@/icons/i18n.icon.svg'
 import { model, storage_keys } from '@/model.ts'
 
@@ -48,8 +50,6 @@ export function LanguageSelect () {
             arrow
             className='header-settings'
         >
-            <div>
-                <SvgI18n className='header-settings-icon'/>
-            </div>
+            <TranslationOutlined className='header-settings-icon' />
         </Dropdown>
 }
