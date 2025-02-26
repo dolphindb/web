ebf14f8c9446bea35721fc3bbd341c5dde1fff3b
Tianchen Fu <tianchen.fu@dolphindb.com>
2025-02-26 10:09:49 +0800

    更新 graph-config.ts 中的类型导入，移除对 Formily 的依赖

--- src/deps.ts
+++ src/deps.ts
@@ -1,5 +1,1 @@
 export * as AntdProComponents from '@ant-design/pro-components'
-
-export * as FormilyCore from '@formily/core'
-export * as FormilyReact from '@formily/react'
-export * as FormilyAntdV5 from '@formily/antd-v5'
