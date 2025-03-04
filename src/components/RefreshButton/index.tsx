c8e3a6619ed380c7391ad80a7dbc1694a4d3b526
Hongfei Shen <hongfei.shen@dolphindb.com>
2025-03-04 16:54:48 +0800

    .

--- src/components/RefreshButton/index.tsx
+++ src/components/RefreshButton/index.tsx
@@ -1,9 +1,10 @@
-import { UndoOutlined } from '@ant-design/icons'
-import { t } from '@i18n/index.ts'
 import { Button, type ButtonProps } from 'antd'
+import { ReloadOutlined } from '@ant-design/icons'
+
+import { t } from '@i18n/index.ts'
 
 export function RefreshButton (props: ButtonProps) {
-    return <Button {...props} icon={<UndoOutlined />}>
+    return <Button {...props} icon={<ReloadOutlined />}>
         {t('刷新')}
     </Button>
 }
