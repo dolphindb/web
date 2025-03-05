21d5f2fd62a024b9e966fc2c68a1b56621978579
Yuke Ying <yuke.ying@dolphindb.com>
2025-03-05 11:33:02 +0800

    feat：bottom-fixed-footer 自适应  sider 宽度

--- src/components/BottomFixedFooter/index.tsx
+++ src/components/BottomFixedFooter/index.tsx
@@ -1,10 +1,21 @@
+import { useSize } from 'ahooks'
 import './index.scss'
-import type { HTMLAttributes } from 'react'
+import { useLayoutEffect, useState, type HTMLAttributes } from 'react'
 
 /** 页面里底部固定按钮，比如页面表单很长，然后表单滚动，按钮固定 */
 export function BottomFixedFooter (props: HTMLAttributes<HTMLDivElement>) {
+    
+    const [sider, setSider] = useState<Element>()
+    
+    // 监听 sider 宽度变更
+    useLayoutEffect(() => { setSider(document.querySelector('.sider')) })
+    const size = useSize(sider) ?? { width: 0 }
+    
     return <>
-        <div className='bottom-fixed-placeholder' />
-        <div className='bottom-fixed-footer' {...props} />
+        <div className='bottom-fixed-placeholder'/>
+        <div 
+            className='bottom-fixed-footer' {...props} 
+            style={{ width: `calc(100% - ${size.width}px)`, left: size.width + 'px' }}
+        />
     </>
 }
