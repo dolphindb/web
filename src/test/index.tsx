acee3b1d6924144e95bdff4678e36475ada5d767
Hongfei Shen <hongfei.shen@dolphindb.com>
2025-03-07 11:21:27 +0800

    svg;

--- src/test/index.tsx
+++ src/test/index.tsx
@@ -1,24 +1,38 @@
 import './index.sass'
 
-import { model } from '@/model.js'
+import { useEffect, useRef } from 'react'
 
-import { StreamingTable } from '@/obj.js'
-// import { StreamingTest } from '../streaming/StreamingTest.js'
+import { delay } from 'xshell/utils.browser.js'
+
+import background_svg from './background.raw.svg'
 
 
 export function Test () {
-    const { ddb: { url, username, password } } = model
+    let rdiv = useRef<HTMLDivElement>(undefined)
     
-    return <div className='obj-result themed page'>
-        <StreamingTable
-            ctx='page'
-            url={url}
-            username={username}
-            password={password}
-            on_error={error => {
-                model.show_error({ error })
-            }}
-        />
-        {/* <StreamingTest /> */}
-    </div>
+    useEffect(() => {
+        let { current: $div } = rdiv
+        
+        let $svg = $div.firstElementChild as SVGSVGElement
+        
+        let $texts: SVGTextElement[] = Array.prototype.filter.call(
+            $svg.children, 
+            ($e: SVGElement) => $e.tagName === 'text' && $e.id)
+        
+        ;(async () => {
+            for (;;) {
+                await delay(3000)
+                
+                $texts.forEach($text => 
+                    $text.textContent = (Math.random() * 100).toFixed()
+                )
+            }
+        })()
+        
+        
+        // let $text = $svg.getElementById('r1_1')
+        // $text.textContent = '啊啊啊啊啊啊啊啊啊啊啊啊'
+    }, [ ])
+    
+    return <div ref={rdiv} dangerouslySetInnerHTML={{ __html: background_svg }} />
 }
