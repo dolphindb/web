59776cd54cccf9d644a0e148842c9571a4696f8d
Chengchen Zheng <chengchen.zheng@dolphindb.com>
2025-04-15 11:23:33 +0800

    api name change

--- src/streaming-graph/apis.ts
+++ src/streaming-graph/apis.ts
@@ -26,16 +26,16 @@ export async function getStreamGraphInfo (name: string): Promise<StreamGraphInfo
 
 
 export async function getCheckpointConfig (name: string): Promise<object> {
-    return model.ddb.invoke('getCheckpointConfig', [ name ])
+    return model.ddb.invoke('getOrcaCheckpointConfig', [ name ])
 }
 
 export async function getCheckpointJobInfo (name: string): Promise<CheckpointJobInfo[]> {
-    const res = await model.ddb.invoke('getCheckpointJobInfo', [ name ])
+    const res = await model.ddb.invoke('getOrcaCheckpointJobInfo', [ name ])
     return res.data
 }
 
 export async function getCheckpointSubjobInfo (name: string): Promise<CheckpointSubjobInfo[]> {
-    const res = await model.ddb.invoke('getCheckpointSubjobInfo', [ name ])
+    const res = await model.ddb.invoke('getOrcaCheckpointSubjobInfo', [ name ])
     return res.data
 }
 
@@ -44,7 +44,7 @@ export async function defGetTaskSubWorkerStat (): Promise<void> {
         getStat = def (): getStreamingStat().subWorkers
         nodes = exec name from getClusterPerf() where (mode=3 or mode=0) and state == 1
         stat = pnodeRun(getStat, nodes)
-        sub = getStreamTaskSubscriptionMeta(name)
+        sub = getOrcaStreamTaskSubscriptionMeta(name)
         return select * from sub, stat where strFind(stat.topic, sub.tableName + "/" + sub.actionName) != -1 order by taskId
     }`)
 }
