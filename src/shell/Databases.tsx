50d808d47b610cb9313c61ae793562f3271f72b5
Tianchen Fu <tianchen.fu@dolphindb.com>
2025-04-25 10:23:49 +0800

    fix: to_dict to data()

--- src/shell/Databases.tsx
+++ src/shell/Databases.tsx
@@ -1469,7 +1469,7 @@ export class ColumnRoot implements DataNode {
             <div className='add-column-button' onClick={async event => {
                 event.stopPropagation()
                 await this.table.db.get_schema()
-                const engineType = this.table.db.schema.to_dict().engineType?.value as string
+                const engineType = this.table.db.schema.data<{ [key: string]: string }>().engineType
                 if (engineType === 'PKEY') {
                     model.modal.error({ title: t('PKEY 数据库引擎不支持 addColumn') })
                     return
