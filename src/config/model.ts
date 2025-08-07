4b1a8062fc14d92dc5475d6a841a80b603ff560b
Hongfei Shen <hongfei.shen@dolphindb.com>
2025-08-07 15:14:32 +0800

    配置管理 ，期望增加两个StreamingSQL相关配置项 WEB-1775

--- src/config/model.ts
+++ src/config/model.ts
@@ -162,7 +162,10 @@ class ConfigModel extends Model<ConfigModel> {
             [t('磁盘')]: new Set(['batchJobDir', 'chunkMetaDir', 'dataSync', 'jobLogFile', 'logFile', 'logLevel', 'maxLogSize', 'redoLogDir', 'redoLogPurgeInterval', 'redoLogPurgeLimit', 'volumes', 'diskIOConcurrencyLevel', 'PKEYMetaLogDir', 'PKEYRedoLogDir', 'coldVolumes']),
             [t('网络')]: new Set(['enableHTTPS', 'localSite', 'maxConnections', 'maxConnectionPerSite', 'tcpNoDelay']),
             [t('流发布')]: new Set(['maxMsgNumPerBlock', 'maxPersistenceQueueDepth', 'maxPubQueueDepthPerSite', 'maxPubConnections', 'persistenceDir', 'persistenceWorkerNum']),
-            [t('流订阅')]: new Set(['maxSubConnections', 'maxSubQueueDepth', 'persistOffsetDir', 'subExecutorPooling', 'subExecutors', 'subPort', 'subThrottle', 'streamingHAMode']),
+            [t('流订阅')]: new Set([
+                'maxSubConnections', 'maxSubQueueDepth', 'persistOffsetDir', 'subExecutorPooling', 'subExecutors', 
+                'subPort', 'subThrottle', 'streamingHAMode', 'streamingSQLExecutors', 'maxStreamingSQLQueriesPerTable'
+            ]),
             [t('系统')]: new Set(['console', 'config', 'home', 'maxPartitionNumPerQuery', 'mode', 'moduleDir', 'newValuePartitionPolicy', 'perfMonitoring', 'pluginDir', 'preloadModules', 'init', 'startup', 'run', 'tzdb', 'webRoot', 'webLoginRequired', 'enableShellFunction', 'enablePKEYEngine']),
             [t('计算组')]: new Set([
                 'computeNodeCacheDir',
@@ -170,7 +173,7 @@ class ConfigModel extends Model<ConfigModel> {
                 'computeNodeMemCacheSize',
                 'computeNodeDiskCacheSize',
                 'enableComputeNodeCacheEvictionFromQueryThread',
-            ]),
+            ])
         }
     }
     
@@ -207,8 +210,7 @@ class ConfigModel extends Model<ConfigModel> {
             'metricsToken',
             'strictPermissionMode',
             'enableLocalDatabase',
-            // 先不发，等 3.00 大版本再发
-            // 'enableClientAuth',
+            'enableClientAuth',
             'computeNodeCachingDelay',
             'computeNodeCachingQueryThreshold',
             'enableComputeNodePrefetchData',
