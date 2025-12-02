abbb3681bda550b1ee8b0b21a153e6b20fcab9e7
Hongfei Shen <hongfei.shen@dolphindb.com>
2025-12-02 11:47:20 +0800

    fix crash;

--- src/shell/QueryGuide/components/QueryForm.tsx
+++ src/shell/QueryGuide/components/QueryForm.tsx
@@ -177,7 +177,7 @@ export function QueryCard (props: IQueryCard) {
                                 <FormDependencies dependencies={[concat_name_path(name_path, name, field.name, 'col'), concat_name_path(name_path, name, field.name, 'opt')]}>
                                     {value => {
                                         const item = name_path ? value?.[name_path]?.[name]?.[field.name] : value?.[name]?.[field.name]
-                                        const { col, opt } = item
+                                        const { col, opt } = item || { }
                                         const { data_type } = safe_json_parse(col ?? '{}')
                                         
                                         
