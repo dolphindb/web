4654b944ad32ceecc2db4bc104d6e1a022674a94
Hongfei Shen <hongfei.shen@dolphindb.com>
2025-12-12 14:26:13 +0800

    rm streaming/;

--- src/streaming/StreamingError.tsx
+++
@@ -1,9 +0,0 @@
-import { type ErrorType } from './types.js'
-import './index.sass'
-
-export function StreamingError ({ error }: { error: ErrorType }) {
-    if (error.appear) 
-        return <div className='streaming_error'>{error.msg}</div>
-    
-    return null
-}
