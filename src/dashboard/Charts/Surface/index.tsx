c7d300c56544d6fec046652c451c4a1e79204fdf
Hongfei Shen <hongfei.shen@dolphindb.com>
2026-01-21 15:47:49 +0800

    .

--- src/dashboard/Charts/Surface/index.tsx
+++ src/dashboard/Charts/Surface/index.tsx
@@ -1,33 +1,37 @@
 import { Collapse, Form, Input } from 'antd'
-import { useEffect, useState } from 'react'
+import { useRef } from 'react'
 
 import { t } from '@i18n'
 import { model } from '@model'
-import type { GraphComponentProps } from '@/dashboard/graphs.ts'
+import type { GraphComponentProps, GraphConfigProps } from '@/dashboard/graphs.ts'
 import { TitleFields } from '@/dashboard/ChartFormFields/components/Title.tsx'
 import { Surface, axises, get_plotlyjs, type SurfaceOptions } from '@components/Surface.tsx'
 
 
-
-export function DashboardSurface ({ widget, data_source }: GraphComponentProps) {
-    let [options, set_options] = useState<SurfaceOptions>({ })
+export function DashboardSurface ({ widget: { config = { } as SurfaceOptions }, data_source }: GraphComponentProps) {
+    let rconfig = useRef<SurfaceOptions>({ })
+    let roptions = useRef<SurfaceOptions>({ })
     
-    useEffect(() => {
-        set_options({
-            ... widget.config,
+    // widget.config 更新后，重新计算 options，不用 useState 避免无效渲染
+    // widget.config 未更新时，复用之前计算的 roptions，保证传递给 Surface 的 options 引用不变
+    if (config !== rconfig.current) {
+        roptions.current = {
+            ... config,
             ... model.shf ? { font: 'MyFont' } : { }
-        })
-    }, [widget.config])
+        }
+        
+        rconfig.current = config as SurfaceOptions
+    }
     
     return <Surface
         // data_source.data 是 { data: number[][], row_labels: string[], col_labels: string[] }
         data={(data_source.data as any)?.data}
-        options={options}
+        options={roptions.current}
         plotlyjs={get_plotlyjs(model.assets_root)} />
 }
 
 
-export function SurfaceConfig () {
+export function SurfaceConfig (props: GraphConfigProps) {
     return <Collapse
         items={[
             {
