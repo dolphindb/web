040f78cb5bb80a07cdf15f9fffad3c4238f242c3
Yuke Ying <yuke.ying@dolphindb.com>
2026-01-28 15:08:20 +0800

    WEB-2014 WEB-2015

--- src/session-management/index.tsx
+++ src/session-management/index.tsx
@@ -1,21 +1,23 @@
 import './index.scss'
 import useSWR from 'swr'
 
-import { useCallback, useState } from 'react'
-import { Popconfirm, Result, Select, Typography } from 'antd'
+import { useCallback, useMemo, useState } from 'react'
+import { Form, Popconfirm, Result, Select, Typography } from 'antd'
 
 
 import { genid } from 'xshell/utils.browser'
 
 import { DdbLong } from 'dolphindb/browser'
 
-import { sum } from 'lodash'
+import { sum, uniq } from 'lodash'
 
 import dayjs from 'dayjs'
 
 import { t } from '@i18n'
 
 import { DDBTable } from '@components/DDBTable/index.tsx'
+
+import { upper } from '@utils'
 
 import { model, NodeType } from '@/model.ts'
 import { Unlogin } from '@/components/Unlogin.tsx'
@@ -29,10 +31,11 @@ interface SessionItem {
     remotePort?: string
     createTime: string
     lastActiveTime?: string
+    type: 'system' | 'user'
 }
 
 
-const script = `nodes=exec name from rpc(getControllerAlias(),getClusterPerf) where mode not in (1,2) and state=1
+const controller_script = `nodes=exec name from rpc(getControllerAlias(),getClusterPerf) where mode not in (1,2) and state=1
 if (count(nodes)>1){
     nodesSessionMemoryStat=pnodeRun(getSessionMemoryStat,nodes)
 }
@@ -43,33 +46,37 @@ crtSessionMemoryStat=rpc(getControllerAlias(),getSessionMemoryStat)
 update crtSessionMemoryStat set node=getControllerAlias()
 nodesSessionMemoryStat.append!(crtSessionMemoryStat)
 nodesSessionMemoryStat
-`
+` 
+
+interface FilterFormValues {
+    type: 'all' | 'system' | 'user'
+    /** 仅控制节点可选择节点筛选 */
+    nodes?: string[]
+}
 
 
 export function SessionManagement () {
     
     const { admin, logined, node_type } = model.use(['admin', 'logined', 'node_type'])
     
+    const [form] = Form.useForm<FilterFormValues>()
+    
     /** 控制节点展所有节点的 session 信息，其他节点仅展示当前节点的 session 信息 */
     const is_controller = node_type === NodeType.controller
-    const [filtered_data, set_filtered_data] = useState<SessionItem[]>([ ])
-    const [type, set_type] = useState<'all' | 'system' | 'user'>('all')
+    const [filter, set_filter] = useState<FilterFormValues>({ type: 'all' })
+    
     
     const { data = [ ], isLoading, mutate } = useSWR(
         admin ? 'session_list' : null,
         async () => {
             const res = is_controller 
-                ? await model.ddb.execute<SessionItem[]>(script)
+                ? await model.ddb.execute<SessionItem[]>(controller_script)
                 : await model.ddb.invoke<SessionItem[]>('getSessionMemoryStat')
             const data = (res ?? [ ]).map(item => ({
                 ...item,    
-                type: item.sessionId ? 'user' : 'system'
+                type: item.sessionId ? 'user' as const : 'system' as const
             }))
-            if (['system', 'user'].includes(type))
-                set_filtered_data(data.filter(item => item.type === type))
-            else
-                set_filtered_data(data)
-            return data
+            return data 
         }
     )
     
@@ -81,6 +88,24 @@ export function SessionManagement () {
         model.message.success(t('会话（ID: {{id}}）关闭成功', { id: session.sessionId.toString() }))
         mutate()
     }, [ ])
+    
+    const filter_data = useMemo(() => {
+        const { type = 'all', nodes = [ ] } = filter
+        
+        return data.filter(item => {
+            // 类型过滤
+            if (type !== 'all' && item.type !== type)
+                return false
+            
+            // 节点过滤（仅控制节点需要）
+            if (is_controller && nodes.length > 0 && !nodes.includes(item.node))
+                return false
+            
+            return true
+        })
+    }, [data, filter, is_controller])
+    
+    
     
      
     if (!logined)
@@ -95,26 +120,31 @@ export function SessionManagement () {
             className='session-management-table'
             filter_form={
                 <>
-                   {t('会话类型：')}
-                   <Select 
-                        onSelect={type => {
-                            set_type(type as 'all' | 'system' | 'user')
-                            if (['system', 'user'].includes(type))
-                                set_filtered_data(data.filter(item => item.type === type))
-                            else
-                                set_filtered_data(data)
-                        }}
-                        className='session-form-select' 
-                        defaultValue='all'
-                        options={[
-                            { label: t('所有会话'), value: 'all' },
-                            { label: t('系统缓存'), value: 'system' }, 
-                            { label: t('用户会话'), value: 'user' }
-                        ]}
-                    />
+                    <Form initialValues={{ type: 'all' }} layout='inline' form={form} onValuesChange={(_, values) => { set_filter(values) } }>
+                        <Form.Item name='type' label={t('会话类型')}>
+                            <Select 
+                                className='session-form-select' 
+                                defaultValue='all'
+                                options={[
+                                    { label: t('所有会话'), value: 'all' },
+                                    { label: t('系统缓存'), value: 'system' }, 
+                                    { label: t('用户会话'), value: 'user' }
+                                ]}
+                            />
+                        </Form.Item>
+                        {is_controller && <Form.Item name='nodes' label={t('节点')}>
+                            <Select 
+                                allowClear
+                                className='session-form-select' 
+                                mode='multiple'
+                                options={ uniq(data.map(item => item.node)).map(node => ({ label: node, value: node }))}
+                            />
+                        </Form.Item>}
+                    </Form>
+                   
                 
                 <div className='session-summary'>
-                    {t('共 {{count}} 条会话，总占用内存 {{memory}}', { count: filtered_data.length, memory: sum(filtered_data.map(item => Number(item.memSize))).to_fsize_str() })}
+                    {t('共 {{count}} 条会话，总占用内存 {{memory}}', { count: filter_data.length, memory: upper(sum(filter_data.map(item => Number(item.memSize))).to_fsize_str()) })}
                 </div>
             </>
             }
@@ -122,7 +152,7 @@ export function SessionManagement () {
             scroll={{ x: '100%', y: 'calc(100vh - 280px)' }}
             rowKey={() => genid()}
             loading={isLoading}
-            dataSource={filtered_data} 
+            dataSource={filter_data} 
             pagination={{
                 defaultPageSize: 20
             }}
@@ -150,7 +180,7 @@ export function SessionManagement () {
                     title: t('占用内存'),
                     dataIndex: 'memSize',
                     width: 180,
-                    render: (value: bigint) => value?.to_fsize_str(),
+                    render: (value: bigint) =>  upper(Number(value).to_fsize_str()),
                     sorter: (a: SessionItem, b: SessionItem) => Number(a.memSize - b.memSize),
                 },
                 {
